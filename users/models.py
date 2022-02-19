b80fba5550c22d1928b4be761b7ef2dda67fc2c2
Valentin Butorin <arch1_cs@mail.ru>
2022-02-19 17:59:46 +0800

    Подготовка к переносу подели пользователя

--- users/models.py
+++ users/models.py
@@ -12,133 +12,129 @@ from random import random
 
 
 class User(AbstractUser):
-    email = models.EmailField(
-        unique=True,
-        error_messages={
-            'unique': "Пользователь с таким адресом эл. почты уже существует.",
-        }
-    )
-    patronymic = models.CharField(
-        max_length=150,
-        blank=True,
-        verbose_name='Очество'
-    )
-    activation_key = models.CharField(
-        max_length=128,
-        blank=True
-    )
-    activation_key_expires = models.DateTimeField(
-        blank=True,
-        null=True
-    )
-    password_recovery_key = models.CharField(
-        max_length=128,
-        blank=True
-    )
-    password_recovery_key_expires = models.DateTimeField(
-        blank=True,
-        null=True
-    )
+    class Meta:
+        db_table = 'auth_user'
 
     def __str__(self):
         return self.username
 
-    def deactivate_user(self):
-        self.is_active = False
-        self.save()
 
-    def activate_user(self):
-        self.is_active = True
-        self.save()
-
-    def generate_activation_key(self):
-        self.activation_key = sha1(str(random()).encode('utf8')).hexdigest()
-        self.activation_key_expires = timezone.now() + timezone.timedelta(hours=48)
-        self.save()
-
-    def is_activation_key_expired(self):
-        if timezone.now() < self.activation_key_expires:
-            return False
-        return True
-
-    def send_verify_mail(self):
-        verify_link = reverse('profiles:verify', args=[self.username, self.activation_key])
-        subject = 'Активация аккаунта'
-        message = f'Чтобы активировать аккаунт, перейдите по ссылке: ' \
-                  f'{settings.BASE_URL}{verify_link}'
-        return send_mail(subject, message, f'Дерево знаний <{settings.EMAIL_HOST_USER}>', [self.email])
-
-    def verify(self, username: str, activation_key: str) -> bool:
-        if self.username == username \
-                and self.activation_key == activation_key \
-                and not self.is_activation_key_expired():
-            self.activate_user()
-            self.activation_key = ''
-            self.activation_key_expires = None
-            self.save()
-            self.save()
-            return True
-        return False
-
-    def generate_password_recovery_key(self):
-        self.password_recovery_key = sha1(str(random()).encode('utf8')).hexdigest()
-        self.password_recovery_key_expires = timezone.now() + timezone.timedelta(hours=48)
-        self.save()
-
-    def is_password_recovery_key_expired(self):
-        if timezone.now() < self.password_recovery_key_expires:
-            return False
-        return True
-
-    def send_password_recovery_mail(self):
-        recovery_link = reverse('profiles:password-recovery-link', args=[self.email, self.password_recovery_key])
-        subject = 'Восстановление пароля'
-        message = f'Для восстановления пароля, перейдите по ссылке: ' \
-                  f'{settings.BASE_URL}{recovery_link}'
-        return send_mail(subject, message, f'Дерево знаний <{settings.EMAIL_HOST_USER}>', [self.email])
-
-    def recovery_valid(self, email: str, key: str):
-        if self.email == email \
-                and self.password_recovery_key == key \
-                and not self.is_password_recovery_key_expired():
-            return True
-        return False
-
-
-class Profile(models.Model):
-    MALE = 'M'
-    FEMALE = 'F'
-    UNKNOWN = 'U'
-
-    GENDERS = (
-        (MALE, 'Мужской'),
-        (FEMALE, 'Женский'),
-        (UNKNOWN, 'Не указан'),
-    )
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    gender = models.CharField(
-        max_length=1,
-        choices=GENDERS,
-        default=UNKNOWN,
-        verbose_name='Пол'
-    )
-    birthday_at = models.DateField(
-        null=True,
-        blank=True,
-        verbose_name='Дата рождения'
-    )
-    avatar = models.ImageField(
-        upload_to='avatars',
-        blank=True, null=True,
-        verbose_name='Аватар'
-    )
-
-    def __str__(self):
-        return f'{self.user.username}\'s profile'
-
-    @receiver(post_save, sender=User)
-    def create_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance).save()
+# class Profile(models.Model):
+#     MALE = 'M'
+#     FEMALE = 'F'
+#     UNKNOWN = 'U'
+#
+#     GENDERS = (
+#         (MALE, 'Мужской'),
+#         (FEMALE, 'Женский'),
+#         (UNKNOWN, 'Не указан'),
+#     )
+#
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#
+#     gender = models.CharField(
+#         max_length=1,
+#         choices=GENDERS,
+#         default=UNKNOWN,
+#         verbose_name='Пол'
+#     )
+#     birthday_at = models.DateField(
+#         null=True,
+#         blank=True,
+#         verbose_name='Дата рождения'
+#     )
+#     avatar = models.ImageField(
+#         upload_to='avatars',
+#         blank=True, null=True,
+#         verbose_name='Аватар'
+#     )
+#     patronymic = models.CharField(
+#         max_length=150,
+#         blank=True,
+#         verbose_name='Очество'
+#     )
+#     activation_key = models.CharField(
+#         max_length=128,
+#         blank=True
+#     )
+#     activation_key_expires = models.DateTimeField(
+#         blank=True,
+#         null=True
+#     )
+#     password_recovery_key = models.CharField(
+#         max_length=128,
+#         blank=True
+#     )
+#     password_recovery_key_expires = models.DateTimeField(
+#         blank=True,
+#         null=True
+#     )
+#
+#     def __str__(self):
+#         return f'{self.user.username}\'s profile'
+#
+#     @receiver(post_save, sender=User)
+#     def create_profile(sender, instance, created, **kwargs):
+#         if created:
+#             Profile.objects.create(user=instance).save()
+#
+#     def deactivate_user(self):
+#         self.is_active = False
+#         self.save()
+#
+#     def activate_user(self):
+#         self.is_active = True
+#         self.save()
+#
+#     def generate_activation_key(self):
+#         self.activation_key = sha1(str(random()).encode('utf8')).hexdigest()
+#         self.activation_key_expires = timezone.now() + timezone.timedelta(hours=48)
+#         self.save()
+#
+#     def is_activation_key_expired(self):
+#         if timezone.now() < self.activation_key_expires:
+#             return False
+#         return True
+#
+#     def send_verify_mail(self):
+#         verify_link = reverse('profiles:verify', args=[self.user.username, self.activation_key])
+#         subject = 'Активация аккаунта'
+#         message = f'Чтобы активировать аккаунт, перейдите по ссылке: ' \
+#                   f'{settings.BASE_URL}{verify_link}'
+#         return send_mail(subject, message, f'Дерево знаний <{settings.EMAIL_HOST_USER}>', [self.user.email])
+#
+#     def verify(self, username: str, activation_key: str) -> bool:
+#         if self.user.username == username \
+#                 and self.activation_key == activation_key \
+#                 and not self.is_activation_key_expired():
+#             self.activate_user()
+#             self.activation_key = ''
+#             self.activation_key_expires = None
+#             self.save()
+#             self.save()
+#             return True
+#         return False
+#
+#     def generate_password_recovery_key(self):
+#         self.password_recovery_key = sha1(str(random()).encode('utf8')).hexdigest()
+#         self.password_recovery_key_expires = timezone.now() + timezone.timedelta(hours=48)
+#         self.save()
+#
+#     def is_password_recovery_key_expired(self):
+#         if timezone.now() < self.password_recovery_key_expires:
+#             return False
+#         return True
+#
+#     def send_password_recovery_mail(self):
+#         recovery_link = reverse('profiles:password-recovery-link', args=[self.user.email, self.password_recovery_key])
+#         subject = 'Восстановление пароля'
+#         message = f'Для восстановления пароля, перейдите по ссылке: ' \
+#                   f'{settings.BASE_URL}{recovery_link}'
+#         return send_mail(subject, message, f'Дерево знаний <{settings.EMAIL_HOST_USER}>', [self.user.email])
+#
+#     def recovery_valid(self, email: str, key: str):
+#         if self.user.email == email \
+#                 and self.password_recovery_key == key \
+#                 and not self.is_password_recovery_key_expired():
+#             return True
+#         return False
