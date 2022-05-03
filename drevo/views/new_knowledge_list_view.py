e260224648abb641c316073703d988976e27656c
Georgyyx <julyfortune101@gmail.com>
2022-05-03 20:39:12 +0300

    new_knowledge view and template, added view in url

---
+++ drevo/views/new_knowledge_list_view.py
@@ -0,0 +1,27 @@
+import datetime
+
+from django.views.generic import ListView
+
+from drevo.models import Znanie
+
+
+class NewKnowledgeListView(ListView):
+    """
+    полный список недавно опубликованных знаний
+    """
+    template_name = 'drevo/new_knowledge.html'
+    model = Znanie
+    context_object_name = 'categorized_new_knowledges'
+
+    def get_queryset(self):
+        # knowledge_later_than = self.request.GET.get('date_point')
+        # if knowledge_later_than
+
+        last_knldgs = Znanie.objects.filter(date__gt=datetime.date.today() - datetime.timedelta(days=7))
+        ctgrs = [knldg.category for knldg in last_knldgs]
+        nstd_l = {}
+        for ctgr in ctgrs:
+            nstd_l[ctgr] = []
+        for n_k in last_knldgs:
+            nstd_l[n_k.category].append(n_k)
+        return nstd_l
