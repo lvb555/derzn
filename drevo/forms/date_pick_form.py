13771d52eb35823d87974c70080c1ff17a6a8651
Georgyyx <julyfortune101@gmail.com>
2022-05-11 22:31:55 +0300

    added form with issue 139 requirements

--- drevo/forms/date_pick_form.py
+++ drevo/forms/date_pick_form.py
@@ -5,20 +5,22 @@ from django.core.exceptions import ValidationError
 
 
 class DatePickNewForm(forms.Form):
+    """Форма поиска знаний по дате публикации"""
     week_ago = datetime.date.today() - datetime.timedelta(days=7)
 
     date = forms.DateField(
         widget=forms.DateInput(attrs={
             'type': 'date',
-            # 'class': 'datepicker',
-            'value': week_ago.strftime('%d-%m-%Y')},
+            'class': 'datepicker',
+            'value': week_ago.strftime('%Y-%m-%d'),
+        },
         ),
         label='Показать знания, начиная с:',
     )
 
     def clean(self):
-        cleaned_data = super().clean()
+        """Форма обрабатывает только прошедшее время"""
         _today = datetime.date.today()
-        date_picked = cleaned_data.get('date')
-        if date_picked > _today:
+        date_picked = self.cleaned_data.get('date')
+        if date_picked and date_picked > _today:
             raise ValidationError('Дату можно изменить.')
--- drevo/views/new_knowledge_list_view.py
+++ drevo/views/new_knowledge_list_view.py
@@ -21,22 +21,16 @@ class NewKnowledgeListView(ListView):
     """
     полный список недавно опубликованных знаний
     """
-    # template_name = 'drevo/new_knowledge.html'
     model = Znanie
     template_name = 'drevo/new_knowledge_date_table.html'
     context_object_name = 'categorized_new_knowledges'
-    # no practical use
-    # form_class = DateNewForm
-    # success_url = ''
 
     def get_queryset(self):
-        date_form = DatePickNewForm(self.request.GET or None)
+        """по запросу из поля даты или за неделю
+        :return Dict[category, List[knowledge]]"""
+        date_form = DatePickNewForm(self.request.GET)
         date_for_new = datetime.date.today() - datetime.timedelta(days=7)
         if date_form.is_valid():
-            # year = date_form.cleaned_data.get('year')
-            # month = date_form.cleaned_data.get('month')
-            # day = date_form.cleaned_data.get('day')
-            # date_for_new = datetime.date(year, month, day)
             date_for_new = date_form.cleaned_data.get('date')
         last_knldgs = Znanie.objects.filter(date__gte=date_for_new)
         ctgrs = [knldg.category for knldg in last_knldgs]
@@ -53,10 +47,6 @@ class NewKnowledgeListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        # _get = self.request.GET.dict()
-        context['datepick_form'] = DatePickNewForm()
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     _get = self.request.GET.dict()
-    #     context['datepick_form'] = DateNewForm(initial=_get.dict()) if not _get else DateNewForm(_get)
-    #     return context
+        _get = self.request.GET.dict()
+        context['datepick_form'] = DatePickNewForm(_get)
+        return context
