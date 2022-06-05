8b6c4f61f866eb60de293ae38e4942b6ea43421f
drmariarty <drmariarty00@gmail.com>
2022-06-05 19:16:08 +0300

    Создан сервис для отправки уведомлений

---
+++ drevo/sender.py
@@ -0,0 +1,58 @@
+"""
+Сервис отправки уведомлений
+"""
+
+from django.core.mail import send_mail
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+def send_email(from_address, to_address, subject, html_message, message):
+    """
+    Отправка email писем
+
+    Parameters
+    ----------
+    from_address : str
+        адрес отправителя
+    to_address : str
+        адрес получателя
+    subject : str
+        тема письма
+    html_message : bool
+        формат сообщения
+    message : str
+        тело письма
+
+    Returns
+    -------
+    bool
+    """
+
+    try:
+        validate_email(from_address)
+        validate_email(to_address)
+    except ValidationError:
+        return False
+
+    if not message:
+        return False
+
+    if not subject:
+        subject = ""
+
+    if not format:
+        format = 'text'
+    
+    try:
+        send_mail(
+            subject,
+            message,
+            from_address,
+            [to_address],
+            fail_silently=True,
+            html_message=html_message
+        )
+    except:
+        return False
+    
+    return True
