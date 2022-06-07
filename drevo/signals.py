8e9d7a3ba13b807b0015a4bdc813e3229f6b5d34
Georgyyx <julyfortune101@gmail.com>
2022-06-07 16:16:42 +0300

    signals

--- drevo/signals.py
+++ drevo/signals.py
@@ -2,16 +2,15 @@ import datetime
 
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-from .knowledge import Znanie
 from dz import settings
 
+from drevo.models import Znanie
 
-@receiver(post_save, sender=Znanie)
+
 def notify(sender, instance, created, **kwargs):
     print('in reciever')
-    # if instance.is_published:
+
     message_subject = 'Новое знание'
     message_content = 'Уважаемый {}{}!\n' \
                       f'{datetime.date.today()} было создано новое' \
@@ -21,6 +20,11 @@ def notify(sender, instance, created, **kwargs):
     for addressee in user_to_notify:
         patr = addressee.profile.patronymic or ''
         yield send_mail(message_subject, message_content.format(
-            addressee.first_name, ' ' + patr),
+            addressee.first_name, ' ' + patr),  # 'b@b.com',
+                        # env.str('EMAIL_HOST_USER'),
                         f'Дерево знаний <{settings.EMAIL_HOST_USER}>',
                         addressee.email)
+
+
+post_save.connect(notify, sender=Znanie, dispatch_uid='Knowledge created',
+                  weak=False)
