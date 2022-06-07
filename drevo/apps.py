fb65887535cd08e639312df12ede0de1c0d03657
Georgyyx <julyfortune101@gmail.com>
2022-06-07 16:16:12 +0300

    apps import

--- drevo/apps.py
+++ drevo/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save, pre_save
 
 
 class DrevoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'drevo'
+    #
+
+    def ready(self):
+        import drevo.signals
