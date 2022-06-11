160b1f158f12b4cdd49961962b7c5ab96d509474
GRusl <ffhshdxva@gmail.com>
2022-06-11 12:23:15 +0300

    The selection of favorites has been removed from the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from users.models import User, Profile, Favourite
-
-
-class FavouritesInlined(admin.StackedInline):
-    model = Favourite
-    can_delete = False
-    filter_horizontal = ('favourites', )
+from users.models import User, Profile
 
 
 class ProfileInlined(admin.StackedInline):
@@ -16,7 +10,7 @@ class ProfileInlined(admin.StackedInline):
 
 
 class UserAdmin(BaseUserAdmin):
-    inlines = (ProfileInlined, FavouritesInlined)
+    inlines = (ProfileInlined, )
 
 
 admin.site.register(User, UserAdmin)
