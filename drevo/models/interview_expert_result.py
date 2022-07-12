55de5f75f57a27f5399e855a90ca28539839983b
Intey <ziexe0@gmail.com>
2022-07-12 21:08:05 +0300

    add model for interview result

--- drevo/models/interview_expert_result.py
+++ drevo/models/interview_expert_result.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from drevo.models.knowledge import Znanie
-from django.auth.contrib import get_user_model
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-class InterviewResult(models.Model):
+class InterviewExpertResult(models.Model):
     """
     Интервью с экспертом.
     Цель интервью - уточнение собранных знаний-вопросов/знаний-ответов.
@@ -16,56 +16,91 @@ class InterviewResult(models.Model):
 
     DESCRIPTION_TYPE = (
         ("ARGUM", "Аргумент"),
-        ("KONTR", "Контраргумент")("INVAL", "Некорректность"),
+        ("KONTR", "Контраргумент"),
+        ("INVAL", "Некорректность"),
     )
 
+    STATUSES = (
+        ("APPRVE", "Принят"),
+        ("REJECT", "Не принят"),
+        ("ANSDPL", "Дублирует ответ"),
+        ("RESDPL", "Дублирует предложение"),
+    )
     # реквизит "Эксперт" - указатель на сущность "Пользователи"
     respondent = models.ForeignKey(
-        model=User, on_delete=models.PROTECT, related_name="interview_results"
+        to=User, on_delete=models.PROTECT, related_name="interview_results"
     )
+
     # реквизит "Интервью" - указатель на сущность "Знания". Это метазнание вида
     # "Интервью".
     interview = models.ForeignKey(
-        model=Znanie,
+        to=Znanie,
         on_delete=models.PROTECT,
-        related_name="interview_results",
+        related_name="expert_results",
     )
+
     # реквизит "Вопрос" - указатель на сущность "Знания". Это знание вида
     # "Вопрос". "Тезис". "Вопрос «Да-Нет".
     question = models.ForeignKey(
-        model=Znanie, on_delete=models.PRTECT, related_name="interview_results"
+        to=Znanie, on_delete=models.PROTECT, related_name="as_question_interviews"
     )
+
     # реквизит "Группа ответов" - логический. Аргументы (ответ "Да") – True;
     # Контраргументы (ответ "Нет") – False
     answer_group = models.BooleanField()
+
     # реквизит "Ответ" - указатель на сущность "Знания".
     answer = models.ForeignKey(
-        model=Znanie, on_delete=models.PRTECT, related_name="interview_results"
+        to=Znanie, on_delete=models.PROTECT, related_name="as_answer_interviews"
     )
+
     # реквизит "Некорректная связь" - логический. Некорректная связь – True;
     is_incorrect_relation = models.BooleanField(verbose_name="не корректная связь")
+
     # реквизит "Статус ответа" - логический. Согласен – True; Не согласен -
     # False
     is_agreed = models.BooleanField(
         verbose_name="Эксперт согласен с ответом?", name="согласен"
     )
+
     # реквизит "Вид текста" - Перечисление (Аргумент; Контраргумент;
     # Некорректность)
     comment_type = models.CharField(
         verbose_name="тип комментария", choices=DESCRIPTION_TYPE, max_length=5
     )
+
     # реквизит "Текст дополнения" – текстовый.
     comment = models.TextField()
+
     # реквизит "Дата/время сессии" – дата/время
     updated = models.DateTimeField()
+
     # реквизит "Ответ/Новый ответ" := Вид блока. Если (это блок "Ответ"?; True;
     # False)
     is_new_answer = models.BooleanField(
         verbose_name="Это ответ предложенный экспертом?"
     )
+
     # реквизит "Предложение эксперта" – текстовый. Это текст предложения
     # (нового ответа)
-    new_answer_text = models.BooleanField(verbose_name="новый ответ от эксперта")
+    new_answer_text = models.TextField(verbose_name="новый ответ от эксперта")
+
+    # реквизит "Новый ответ" - указатель на сущность "Знания". Это новый ответ, созданный на основе предложения эксперта.
+    new_answer = models.OneToOneField(
+        to=Znanie, on_delete=models.PROTECT, related_name="made_in_interview"
+    )
+
+    # реквизит "Администратор" - указатель на сущность "Пользователи"
+    admin_reviewer = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    # реквизит "Комментарий администратора" – текстовый
+    admin_comment = models.TextField()
+
+    # реквизит "Статус предложения" – указатель на перечисление «Статусы предложений».
+    status = models.CharField(
+        choices=STATUSES,
+        max_length=6,
+    )
 
     def save(self, *args, **kwargs):
         """On save, update timestamp"""
