7603e11c5a84b0051e5328907220f1e456b45242
Intey <ziexe0@gmail.com>
2022-07-27 13:18:46 +0300

    we can have a proposal to answer, that not contained in interview
    In such case we need a link to this answer. But when expert make a proposal in interview, we need to know in which interview this answer have such proposal

--- drevo/models/interview_answer_expert_proposal.py
+++ drevo/models/interview_answer_expert_proposal.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+
 from drevo.models.knowledge import Znanie
 from django.contrib.auth import get_user_model
 
@@ -36,41 +36,63 @@ class InterviewAnswerExpertProposal(models.Model):
     # реквизит "Группа ответов" - логический. Аргументы (ответ "Да") – True;
     # Контраргументы (ответ "Нет") – False
     is_yesno_answer_argument = models.BooleanField(
-        verbose_name="Аргумент вопроса Да-Нет? (нет - контраргумент)"
+        default=False, verbose_name="Аргумент вопроса Да-Нет? (нет - контраргумент)"
+    )
+
+    # ссылка на интервью
+    interview = models.ForeignKey(
+        to=Znanie,
+        on_delete=models.PROTECT,
+        related_name="interview_proposals",
+        verbose_name="Интервью",
     )
 
     # реквизит "Ответ" - указатель на сущность "Знания".
+    # ссылка на связь между вопросом и интервью. Связь с типом "Состав"
     answer = models.ForeignKey(
         to=Znanie,
+        null=True,
         on_delete=models.PROTECT,
-        related_name="as_answer_interviews",
+        related_name="answer_proposals",
         verbose_name="Ответ",
     )
 
     # реквизит "Некорректная связь" - логический. Некорректная связь – True;
-    is_incorrect_answer = models.BooleanField(verbose_name="некорректный ответ")
+    is_incorrect_answer = models.BooleanField(
+        default=False, verbose_name="некорректный ответ"
+    )
 
     # реквизит "Статус ответа" - логический. Согласен – True; Не согласен -
     # False
     is_agreed = models.BooleanField(
-        verbose_name="Эксперт согласен с ответом?", name="согласен"
+        default=True, verbose_name="Эксперт согласен с ответом?", name="согласен"
     )
 
     # реквизит "Текст дополнения" – текстовый.
     # список аргументов или контраргументов
-    comment = models.JSONField(verbose_name="Аргументы и контраргументы (JSON)")
+    # для получения полей используейте getters ниже (get_arguments, get_counter_arguments)
+    comment = models.JSONField(
+        default=dict, verbose_name="Аргументы и контраргументы (JSON)"
+    )
 
     # реквизит "Дата/время сессии" – дата/время
-    updated = models.DateTimeField()
+    updated = models.DateTimeField(auto_now=True)
 
     # реквизит "Предложение эксперта" – текстовый. Это текст предложения
     # (нового ответа)
-    new_answer_text = models.TextField(verbose_name="новый ответ от эксперта")
+    new_answer_text = models.TextField(
+        null=True, verbose_name="новый ответ от эксперта"
+    )
+
+    # ================================= Admin fields ========================
+    # Следующие поля изменяются уже администратором
+    # Возможно, будет правильноее не заводить их, а использовать текущие
 
     # реквизит "Новый ответ" - указатель на сущность "Знания". Это новый ответ,
     # созданный на основе предложения эксперта.
     new_answer = models.OneToOneField(
         to=Znanie,
+        null=True,
         on_delete=models.PROTECT,
         related_name="made_in_interview",
         verbose_name="Новый ответ",
@@ -78,24 +100,34 @@ class InterviewAnswerExpertProposal(models.Model):
 
     # реквизит "Администратор" - указатель на сущность "Пользователи"
     admin_reviewer = models.ForeignKey(
-        User, on_delete=models.PROTECT, verbose_name="Администратор"
+        User, null=True, on_delete=models.PROTECT, verbose_name="Администратор"
     )
 
     # реквизит "Комментарий администратора" – текстовый
-    admin_comment = models.TextField(verbose_name="Комментарий администратора")
+    admin_comment = models.TextField(
+        default="", verbose_name="Комментарий администратора"
+    )
 
     # реквизит "Статус предложения" – указатель на перечисление «Статусы предложений».
     status = models.CharField(
+        null=True,
         choices=STATUSES,
         max_length=6,
     )
 
-    def save(self, *args, **kwargs):
-        """On save, update timestamp"""
-        # also note this:
-        # https://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add
-        self.modified = timezone.now()
-        return super(User, self).save(*args, **kwargs)
+    @staticmethod
+    def create_new_answer(
+        expert_user, text: str, comment: str = "{}"
+    ) -> "InterviewAnswerExpertProposal":
+        return InterviewAnswerExpertProposal.objects.create(
+            new_answer_text=text, expert=expert_user, comment=comment
+        )
+
+    def get_arguments(self) -> list[str]:
+        return self.comment.get("arguments", [])
+
+    def get_counter_arguments(self) -> list[str]:
+        return self.comment.get("counter_arguments", [])
 
     class Meta:
         verbose_name = "Предложение эксперта"
