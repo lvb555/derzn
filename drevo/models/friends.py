f24741874959750109f174b77df727f568fbe4dd
Руст <rust@iMac-Rust.local>
2022-09-18 10:21:54 +0500

    Реализована задача №20 (Создание раздела друзья) https://github.com/breduin/derzn/issues/152

---
+++ drevo/models/friends.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from users.models import User
+
+
+class FriendsTerm(models.Model):
+    """
+    реквизит "Пользователь" - указатель на таблицу "Пользователи"
+    реквизит "Друг" - указатель на таблицу "Пользователи"
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='user'
+    )
+    friend = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Друг',
+        related_name='friend'
+    )
---
+++ drevo/views/friends_view.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+from ..models import FriendsTerm
+from ..models import FriendsInviteTerm
+from users.models import Profile, User
+
+
+def friends_view(request):
+    """
+    Контрол для страницы "Друзья"
+    """
+    context = {'profiles': []}
+
+    # Загрузим список заявок на дружбу
+    invite_count = FriendsInviteTerm.objects.filter(recipient=request.user.id).count()
+    context['invite_count'] = invite_count if invite_count else 0
+
+    # Удаление из списка друзей
+    if request.GET.get('remove'):
+        _remove_friend(request.user.id, request.GET.get('remove'))
+
+    user_obj = FriendsTerm.objects.filter(user_id=request.user.id).first()
+    if user_obj:
+        profiles = Profile.objects.filter(id=user_obj.friend_id)
+        for profile in profiles:
+            data = {}
+            user = User.objects.filter(id=profile.user_id)
+            data['first_name'] = user[0].first_name
+            data['last_name'] = user[0].last_name
+            data['avatar'] = profile.avatar
+            data['user_id'] = profile.user_id
+            context['profiles'].append(data)
+
+    template_name = 'drevo/friends.html'
+    return render(request, template_name, context)
+
+
+def _remove_friend(user_id, friend_id):
+    """
+    Удалить из друзей
+    """
+    friend_table = FriendsTerm.objects.filter(user_id=user_id, friend_id=friend_id)
+    friend_table.delete()
