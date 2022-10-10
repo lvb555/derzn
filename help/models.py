3b9b4b61b2230de6188810d04df3f119e4736e8c
Антон <a.d.moskalev@yandex.ru>
2022-10-10 22:24:21 +1000

    fix help

--- help/models.py
+++ help/models.py
@@ -1,12 +1,7 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
 
 class HelpPage(models.Model):
-    """
-    Метод save() Добавляет к имени тега '/', если его нет,
-    или убирает если '/' добавлен в конец.
-    """
     header = models.CharField(max_length=50,
                               verbose_name="Заголовок")
     content = models.TextField(verbose_name="Содержание")
@@ -16,12 +11,14 @@ class HelpPage(models.Model):
                                verbose_name="URL тег адреса",)
 
     def save(self, *args, **kwargs):
+        """
+        Добавляет перед url_tag символ "/", если его нет.
+        Убирает символ "/" в конце url_tag, если он есть.
+        """
         if len(self.url_tag) == 0 or self.url_tag[0] != '/':
             self.url_tag = '/' + self.url_tag
         if len(self.url_tag) >= 2 and self.url_tag[-1] == '/':
             self.url_tag = self.url_tag[:-1]
-        if HelpPage.objects.filter(url_tag=self.url_tag).exists():
-            raise ValidationError("Значение поля url_tag должно быть уникальным")
 
         super().save(*args, **kwargs)
 
