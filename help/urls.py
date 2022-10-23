d412061aeec78a8eac3abe213d1a771daa1b8877
Антон <a.d.moskalev@yandex.ru>
2022-10-24 03:25:36 +1000

    revision of the help block

--- help/urls.py
+++ help/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+# from .views import CategoryListView
 
 from . import views
 
 urlpatterns = [
     path("", views.help, name="help"),
+    path("<str:category>", views.help, name="help"),
 ]
