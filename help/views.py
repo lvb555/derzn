ad5601e56a475f7c9c6c14c970b6fa7a7a260dc1
Антон <a.d.moskalev@yandex.ru>
2022-11-03 05:33:03 +1000

    fix help page

--- help/views.py
+++ help/views.py
@@ -4,14 +4,18 @@ from .models import HelpPage, CategoryHelp
 
 
 def help(request, category=None):
-    nodes = CategoryHelp.objects.all()
+    nodes = CategoryHelp.objects.filter(is_published=True)
     if category is not None:
         context = HelpPage.objects.filter(category__url_tag="/" + category, category__is_published=True).first()
         if context:
             return render(request, "help/help.html", {"context": context, "nodes": nodes})
     url_redirect = request.META.get('HTTP_REFERER')
     try:
-        url_tag = re.search(r"(?<=drevo)/\w+|/$", url_redirect).group(0)
+        try:
+            url_tag = re.search(r"(?<=drevo)/\w+|/$", url_redirect).group(0)
+        except AttributeError:
+            context = HelpPage.objects.filter(category__url_tag='/', category__is_published=True).first()
+            return render(request, "help/help.html", {"context": context, "nodes": nodes})
         context = HelpPage.objects.filter(category__url_tag=url_tag, category__is_published=True).first()
         if context:
             return render(request, "help/help.html", {"context": context, "nodes": nodes})
