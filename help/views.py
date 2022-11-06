11fe3d235a4d0045e72fdf8588b0c69177e093cc
Антон <a.d.moskalev@yandex.ru>
2022-11-06 16:56:38 +1000

    fix help_v3

--- help/views.py
+++ help/views.py
@@ -11,14 +11,13 @@ def help(request, category=None):
             return render(request, "help/help.html", {"context": context, "nodes": nodes})
     url_redirect = request.META.get('HTTP_REFERER')
     try:
-        try:
-            url_tag = re.search(r"(?<=drevo)/\w+|/$", url_redirect).group(0)
-        except AttributeError:
-            context = HelpPage.objects.filter(category__url_tag='/', category__is_published=True).first()
-            return render(request, "help/help.html", {"context": context, "nodes": nodes})
+        url_tag = re.search(r"(?<=drevo)/\w+|/$", url_redirect).group(0)
         context = HelpPage.objects.filter(category__url_tag=url_tag, category__is_published=True).first()
         if context:
             return render(request, "help/help.html", {"context": context, "nodes": nodes})
+    except AttributeError:
+        context = HelpPage.objects.filter(category__url_tag='/', category__is_published=True).first()
+        return render(request, "help/help.html", {"context": context, "nodes": nodes})
     except Exception:
         return render(request, "help/help_404.html", {'nodes': nodes})
     return render(request, "help/help_404.html", {'nodes': nodes})
