b19d7e1494827873cdb6b0ff492ec8d2184c1240
Demidov <demidov.evg.v@gmail.com>
2022-11-07 22:35:02 +0300

    fix mark in 'changeInputHandler' and refactor func 'has_group'

--- drevo/templatetags/has_group.py
+++ drevo/templatetags/has_group.py
@@ -1,13 +1,10 @@
-from django.contrib.auth.models import Group
 from django import template
+
+from users.models import User
 
 register = template.Library()
 
 
 @register.filter(name='has_group')
-def has_group(user, group_name):
-    try:
-        group = Group.objects.get(name=group_name)
-        return True if group in user.groups.all() else False
-    except Group.DoesNotExist:
-        return False
+def has_group(user: User, group_name: str) -> bool:
+    return user.groups.filter(name=group_name).exists()
