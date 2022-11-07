a6a033e12115adcb23416565fe4619f33aff5fa1
Demidov <demidov.evg.v@gmail.com>
2022-11-07 21:24:43 +0300

    fix: add tree categories in search knowledges, feature: add command for create developer(who can create dump from db, and read all tables): python manage.py create_developer

--- drevo/forms/select_with_input.py
+++ drevo/forms/select_with_input.py
@@ -4,3 +4,8 @@ from django import forms
 class SelectWithInput(forms.Select):
     template_name = 'drevo/forms/select.html'
     option_template_name = 'drevo/forms/select_option.html'
+
+
+class SelectWithInputTree(forms.Select):
+    template_name = 'drevo/forms/tree_select.html'
+    option_template_name = 'drevo/forms/tree_select_option.html'
