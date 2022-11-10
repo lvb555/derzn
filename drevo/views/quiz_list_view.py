57461cd93eb8aa6d3f2d9377191e68edf85c67fe
Intey <ziexe0@gmail.com>
2022-11-10 08:34:35 +0300

    на этапе анализа кода модели не существует

--- drevo/views/quiz_list_view.py
+++ drevo/views/quiz_list_view.py
@@ -2,16 +2,20 @@ from django.views.generic import ListView
 from ..models import Znanie, Tz
 from loguru import logger
 
-logger.add('logs/main.log',
-           format="{time} {level} {message}", rotation='100Kb', level="ERROR")
+logger.add(
+    "logs/main.log", format="{time} {level} {message}", rotation="100Kb", level="ERROR"
+)
 
 
 class QuizListView(ListView):
     """
     выводит список теcтов
     """
-    template_name = 'drevo/all_quizzes.html'
+
+    template_name = "drevo/all_quizzes.html"
     model = Znanie
-    context_object_name = 'all_tests'
-    test_type = Tz.objects.get(name='Тест')
-    queryset = Znanie.objects.filter(tz=test_type, is_published=True)
+    context_object_name = "all_tests"
+
+    def get_queryset(self):
+        test_type = Tz.objects.get(name="Тест")
+        return Znanie.objects.filter(tz=test_type, is_published=True)
