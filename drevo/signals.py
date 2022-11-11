ce14a08bd73acd5954942e1372cc9ed43cbea768
Intey <ziexe0@gmail.com>
2022-11-11 15:37:39 +0300

    импорт профилей через дамп не работал, т.к. профили создавались в процессе миграции

--- drevo/signals.py
+++ drevo/signals.py
@@ -14,8 +14,9 @@ from django.db import transaction
 from drevo.sender import send_email
 from loguru import logger
 
-logger.add('logs/main.log',
-           format="{time} {level} {message}", rotation='100Kb', level="INFO")
+logger.add(
+    "logs/main.log", format="{time} {level} {message}", rotation="100Kb", level="INFO"
+)
 
 
 # Функция notify, которая передается в on_commit(), будет вызвана сразу после того, как гипотетическая запись в базу данных, сделанная там,
@@ -30,26 +31,31 @@ def on_transaction_commit(func):
 
 @on_transaction_commit
 def notify(sender, instance: Znanie, created, **kwargs):
+    if sender._meta.object_name == "Migrate":
+        breakpoint()
+        return
     """Sends messages with application to author subscribers on knowledge creation"""
     tz_model = instance.__class__.tz.field.remote_field.model
 
-    if (not instance.is_published
-            or not instance.author
-            or not created
-            or not tz_model.objects.filter(id=instance.tz_id).exists()
-            or instance.tz.is_systemic):
+    if (
+        not instance.is_published
+        or not instance.author
+        or not created
+        or not tz_model.objects.filter(id=instance.tz_id).exists()
+        or instance.tz.is_systemic
+    ):
         return
 
     user_to_notify = set(instance.author.subscribers.all())
 
     for knowledge_tags in instance.labels.all():
         # Объеденяем множества. Так у нас не будут повторяться пользовотели, которым нужно отправить уведомление.
-        user_to_notify = (user_to_notify | set(knowledge_tags.subscribers.all()))
+        user_to_notify = user_to_notify | set(knowledge_tags.subscribers.all())
 
     if not user_to_notify:
         return
     author_publication_url = settings.BASE_URL + instance.get_absolute_url()
-    message_subject = 'Новое знание'
+    message_subject = "Новое знание"
     months = {
         1: "января",
         2: "февраля",
@@ -66,19 +72,25 @@ def notify(sender, instance: Znanie, created, **kwargs):
     }
     date_now = datetime.date.today()
     cur_month_formed = months[date_now.month]
-    date_with_month = date_now.strftime(f'%d {cur_month_formed} %Y')
-    message_content = 'Уважаемый {}{}!\n' \
-                      f'{date_with_month} было создано новое' \
-                      f' знание:\n  {author_publication_url}\n' \
-                      f'Автор: {instance.author}\n'
+    date_with_month = date_now.strftime(f"%d {cur_month_formed} %Y")
+    message_content = (
+        "Уважаемый {}{}!\n"
+        f"{date_with_month} было создано новое"
+        f" знание:\n  {author_publication_url}\n"
+        f"Автор: {instance.author}\n"
+    )
     for addressee in user_to_notify:
-        patr = ''
+        patr = ""
         user_profile = addressee.profile
         if addressee.first_name and user_profile.patronymic:
-            patr = ' ' + user_profile.patronymic
-        appeal = addressee.first_name or 'пользователь'
-        send_email(addressee.email, message_subject, False,
-                   message_content.format(appeal, patr))
+            patr = " " + user_profile.patronymic
+        appeal = addressee.first_name or "пользователь"
+        send_email(
+            addressee.email,
+            message_subject,
+            False,
+            message_content.format(appeal, patr),
+        )
 
 
 @receiver(post_save, sender=Relation)
@@ -89,10 +101,17 @@ def notify_new_interview(sender, instance, created, **kwargs):
     """
     # Настраиваем сигнал для срабатывания при создании связи вида "Период интервью"
     # Условия для проверки публикации связи, интервью и периода интервью
-    condition_publish = (instance.is_published, instance.bz.is_published, instance.rz.is_published)
+    condition_publish = (
+        instance.is_published,
+        instance.bz.is_published,
+        instance.rz.is_published,
+    )
 
     # Условия для проверки, что вид связи соответствует "Период интервью" и базовое знание "Интервью"
-    condition_name = (instance.bz.tz.name == 'Интервью', instance.tr.name == 'Период интервью')
+    condition_name = (
+        instance.bz.tz.name == "Интервью",
+        instance.tr.name == "Период интервью",
+    )
 
     # Проверяем первоначальные условия и, если им не соответствуем, завершаем работу сигнала
     if not all((*condition_publish, *condition_name, created)):
