c2c3aa6e32ed88bb8b4fdde85f63e97ac4265b10
sergeybv26 <sergey.bv26@gmail.com>
2022-11-19 11:54:36 +0300

    Скрыты поля, которые не должны редактироваться на определенных этапах ТП

--- drevo/forms/knowledge_create_form.py
+++ drevo/forms/knowledge_create_form.py
@@ -38,7 +38,8 @@ class ZnanieCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != 'is_send':
+                field.widget.attrs['class'] = 'form-control'
 
 
 ZnImageFormSet = inlineformset_factory(
--- drevo/forms/knowledge_update_form.py
+++ drevo/forms/knowledge_update_form.py
@@ -37,7 +37,8 @@ class ZnanieUpdateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != 'is_send':
+                field.widget.attrs['class'] = 'form-control'
             if self.instance.get_current_status.status not in variables.EDIT_STATUS:
                 field.widget.attrs['readonly'] = True
 
