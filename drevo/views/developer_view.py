e71a538e55502bfdb5c8b9e3b27beea4a06ca610
Daniil Melnikov <dnl.melnikov@yandex.ru>
2022-11-27 16:35:02 +0700

    reduce developer_view

--- drevo/views/developer_view.py
+++ drevo/views/developer_view.py
@@ -6,29 +6,12 @@ def developer_view(request):
     Отображаем страницу Разработчики
     """
     filter = request.GET.get("filter")
-    developers_list = Developer.objects.order_by('-contribution')
     filter_ = False
-
     if filter == 'name':
         filter_ = True
-        developers = []
-        for developer in developers_list:
-            developers.append(developer)
-        def swap(i, j):
-            developers[i], developers[j] = developers[j], developers[i]
-
-        n = len(developers)
-        swapped = True
-
-        x = -1
-        while swapped:
-            swapped = False
-            x = x + 1
-            for i in range(1, n-x):
-                if developers[i - 1].name[0] > developers[i].name[0]:
-                    swap(i - 1, i)
-                    swapped = True
-        developers_list = developers
+        developers_list = Developer.objects.order_by('name')
+    else:
+        developers_list = Developer.objects.order_by('-contribution')
 
     context = {
         'developers_list': developers_list,
