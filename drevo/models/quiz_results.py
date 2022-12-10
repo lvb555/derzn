1e459119e74ee701728b26fe66c42ee2d5b4f958
Sofia-Dieva <titlyanova37@mail.ru>
2022-12-10 05:18:38 +0200

    Исправлены замечания, добавлен код в шаблоне для сохраниния результатов и view  соответственно

--- drevo/models/quiz_results.py
+++ drevo/models/quiz_results.py
@@ -4,16 +4,14 @@ from users.models import User
 from drevo.models import Znanie
 
 
-
-
 class QuizResult(models.Model):
     """
     Таблица для хранения результатов теста
     """
-    quiz = models.ForeignKey(Znanie, verbose_name='Тест', related_name='passed_quiz', on_delete=models.DO_NOTHING)
-    question = models.ForeignKey(Znanie, verbose_name='Вопрос', related_name='question_in_quiz', on_delete=models.DO_NOTHING)
-    answer = models.ForeignKey(Znanie, verbose_name='Ответ', related_name='answer_in_quiz', on_delete=models.DO_NOTHING)
-    user = models.ForeignKey(User, verbose_name='Тестируемый', related_name='tested_user', on_delete=models.DO_NOTHING)
+    quiz = models.ForeignKey(Znanie, verbose_name='Тест', related_name='passed_quiz', on_delete=models.CASCADE)
+    question = models.ForeignKey(Znanie, verbose_name='Вопрос', related_name='questions_in_quiz', on_delete=models.CASCADE)
+    answer = models.ForeignKey(Znanie, verbose_name='Ответ', related_name='answers_in_quiz', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Тестируемый', related_name='quiz_results', on_delete=models.CASCADE)
     date_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время решения')
 
     class Meta:
@@ -21,4 +19,4 @@ class QuizResult(models.Model):
         verbose_name_plural = 'Результаты тестов'
 
     def __str__(self):
-        return f'{self.id} - {self.user} - {self.quiz}({self.date_time:%d.%m.%Y})'
+        return f'{self.user} - {self.quiz}({self.date_time:%d.%m.%Y})'
