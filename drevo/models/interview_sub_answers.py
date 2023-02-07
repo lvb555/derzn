58dcaa1b86fd3f51573cd3eb45c2b6db19f0bfd9
acclrtrrr <andrey.novikov2001@yandex.ru>
2023-02-07 21:51:20 +0300

    Создал модель 'Подответы'(SubAnswers)

---
+++ drevo/models/interview_sub_answers.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class SubAnswers(models.Model):
+    """
+    Данная модель хранит подответы для знаний (ответов на вопрос интервью),
+    которые были созданы на основе предложений экспертов
+    """
+    expert = models.ForeignKey(
+        verbose_name='Эксперт',
+        to='users.User',
+        on_delete=models.CASCADE,
+        related_name='sub_answers'
+    )
+    question = models.ForeignKey(
+        verbose_name='Вопрос',
+        to='drevo.Znanie',
+        on_delete=models.CASCADE,
+        related_name='sub_answers'
+    )
+    answer = models.ForeignKey(
+        verbose_name='Ответ',
+        to='drevo.Znanie',
+        on_delete=models.CASCADE,
+        related_name='sub_answers'
+    )
+    sub_answer = models.TextField(
+        verbose_name='Подответ'
+    )
+
+    class Meta:
+        verbose_name = 'Подответ'
+        verbose_name_plural = 'Подответы'
+
+    def __str__(self):
+        return f'SubAnswer #{self.pk}: {self.expert}'
