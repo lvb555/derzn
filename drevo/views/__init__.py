68117bb3b585f087431d87048190fe76a56636e3
Julia <101992203+julant7@users.noreply.github.com>
2023-02-24 12:58:45 +0300

    Задача 334

--- drevo/views/__init__.py
+++ drevo/views/__init__.py
@@ -28,7 +28,7 @@ from .admin_interview_work.views import (
 from .favourite_processing_view import FavouriteProcessView
 from .favourites_view import FavouritesView
 from .filling_tables import filling_tables, get_rows_and_columns, znanie_attributes, show_new_znanie, \
-    show_filling_tables_page
+    show_filling_tables_page, get_form_data
 from .friends_added_view import friends_added_view
 from .friends_view import friends_view
 from .glossary_list_view import GlossaryListView
@@ -81,6 +81,11 @@ __all__ = [
     "FavouritesView",
     "FavouriteProcessView",
     "filling_tables",
+    "get_rows_and_columns",
+    "get_form_data",
+    "znanie_attributes",
+    "show_new_znanie",
+    "show_filling_tables_page",
     "friends_view",
     "friends_added_view",
     "friends_invite_view",
