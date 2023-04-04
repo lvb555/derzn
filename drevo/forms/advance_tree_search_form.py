50fb39b037093b716e53b272c712f5def0c2dbcf
acclrtrrr <andrey.novikov2001@yandex.ru>
2023-04-04 19:22:52 +0300

    Отредактировал форму поиска и сттраницу с результатами

--- drevo/forms/advance_tree_search_form.py
+++ drevo/forms/advance_tree_search_form.py
@@ -1,6 +1,6 @@
 from django import forms
 from mptt.forms import TreeNodeChoiceField
-from drevo.models import Znanie, Author, Category, Tz, Tr, Label
+from drevo.models import Znanie, Author, Tz, Tr, Label
 
 
 class AdvanceTreeSearchFrom(forms.Form):
@@ -10,12 +10,6 @@ class AdvanceTreeSearchFrom(forms.Form):
     knowledge_type = forms.ModelChoiceField(
         label='Вид знания',
         queryset=Tz.objects.filter(is_systemic=False),
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        required=False
-    )
-    category = TreeNodeChoiceField(
-        label='Категория',
-        queryset=Category.tree_objects.all(),
         widget=forms.Select(attrs={'class': 'form-control'}),
         required=False
     )
