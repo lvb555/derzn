a2027e958bd5386a63b1b8126d966d866582d350
Sofia-Dieva <titlyanova37@mail.ru>
2023-04-24 16:02:18 +0200

    Верстка старницы тегов

--- drevo/templatetags/base_tags.py
+++ drevo/templatetags/base_tags.py
@@ -14,3 +14,10 @@ def get_relation_types() -> QuerySet[Tr]:
 def get_knowledge_types() -> QuerySet[Tz]:
     kn_types = Tz.objects.filter(is_systemic=False).order_by('name')
     return kn_types
+
+
+@register.simple_tag
+def url_replace(request, field, value):
+    dict_ = request.GET.copy()
+    dict_[field] = value
+    return dict_.urlencode()
--- drevo/views/labels_list_view.py
+++ drevo/views/labels_list_view.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from ..models import Label
 from ..models import Label
 from loguru import logger
+from django.urls import reverse
 
 
 logger.add('logs/main.log',
@@ -16,18 +17,32 @@ class LabelsListView(ListView):
     template_name = 'drevo/labels.html'
     model = Label
     context_object_name = 'labels'
+    paginate_by = 25
 
     def get_queryset(self):
+        tag_sorted = self.request.GET.get('order')
+        have_to_be_sorted = 'name'
+        if tag_sorted:
+            if tag_sorted == '-count':
+                have_to_be_sorted = 'zn_num'
+            else:
+                have_to_be_sorted = '-zn_num'
         if tag_for_search := self.request.GET.get('tag_for_search'):
             return (
                 Label.objects
                 .annotate(zn_num=Count('znanie', filter=Q(znanie__is_published=True)))
                 .filter(zn_num__gte=1, name__icontains=tag_for_search)
-                .order_by('name')
+                .order_by(have_to_be_sorted)
             )
         return (
             Label.objects
             .annotate(zn_num=Count('znanie', filter=Q(znanie__is_published=True)))
             .filter(zn_num__gte=1)
-            .order_by('name')
+            .order_by(have_to_be_sorted)
         )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if tag_for_search := self.request.GET.get('tag_for_search'):
+            context['tag_for_search'] = tag_for_search
+        return context
