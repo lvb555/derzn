42aeb1a826b7e62e565cb7c90132b32f4d66ccd7
Dinar Fazliev <jjustdinar@gmail.com>
2023-09-03 16:22:00 +0300

    template for suggestions

---
+++ users/forms/suggestion_create_form.py
@@ -0,0 +1,21 @@
+from django import forms
+from drevo.models import Tz, Tr
+
+tz = [(i.id, i.name) for i in Tz.objects.filter(is_systemic=False)]
+tr = [(i.id, i.name) for i in Tr.objects.filter(is_systemic=False)]
+
+tz = tuple([(-1, 'Выберите подходящий тип знания')] + tz)
+tr = tuple([(-1, 'Укажите отношение к текущему знанию')] + tr)
+
+
+
+class SuggestionCreateForm(forms.Form):
+    select_widget = forms.Select(attrs={'class': 'form-control'})
+    text_widget = forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите название вашего знания'})
+
+    parent_knowledge = forms.CharField(max_length=100, widget=forms.HiddenInput())
+    knowledge_type = forms.ChoiceField(choices=tz, label='Тип знания', widget=select_widget)
+    relation_type = forms.ChoiceField(choices=tr, label='Отношение к старшему знанию', widget=select_widget)
+    name = forms.CharField(max_length=256, label='Текст знания', widget=text_widget)
