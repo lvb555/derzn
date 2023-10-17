0c7635332f802bcb3ce0a62af4f46928e21fe58f
Dinar Fazliev <jjustdinar@gmail.com>
2023-10-18 00:35:33 +0300

    linked admin_user_suggestion_form to usersuggestion model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from users.forms.admin_user_suggestion_form import UserSuggestionAdminForm
+
 from users.models import User, Profile, MenuSections, UserSuggection, SuggestionType
 
 
@@ -55,6 +57,8 @@ class UserAdmin(BaseUserAdmin):
             }
         )
     )
+
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_first_name', 'user_last_name', 'user_email', 'gender', 'birthday_at')
@@ -72,15 +76,19 @@ class ProfileAdmin(admin.ModelAdmin):
         return obj.user.email
     user_email.short_description = "Email"
 
+
 @admin.register(MenuSections)
 class MenuSectionsAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ["name"]
 
+
 @admin.register(UserSuggection)
 class UserSuggestionAdmin(admin.ModelAdmin):
     list_display = ('parent_knowlege', 'name', 'user', 'expert', 'is_approve', 'suggestions_type')
     list_filter = ('suggestions_type', 'user', 'parent_knowlege')
+    form = UserSuggestionAdminForm
+
 
 @admin.register(SuggestionType)
 class SuggestionTypeAdmin(admin.ModelAdmin):
