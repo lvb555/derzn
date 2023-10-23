eba9a2747b2c763b65f4ae8fd712511c7c89f4f9
Dinar Fazliev <jjustdinar@gmail.com>
2023-10-23 20:10:54 +0300

    fix(migrations) migrate data from old models into new new models.
    Data-миграция - 0036, перед тем как ее создать, откатил удаление старых моделей(0035). По сути это имеет значение только локальное, ведь на сервере миграция не применялась. Затем переписал dependecies, чтобы сначала выполнилась миграция 0036, и потом 0035

--- users/migrations/0035_auto_20231018_2239.py
+++ users/migrations/0035_auto_20231018_2239.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0034_auto_20231018_2153'),
+        ('users', '0036_auto_20231023_1928'),
     ]
 
     operations = [
