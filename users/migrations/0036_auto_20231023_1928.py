750208d8815f3bf82b526d07f4910cf50a75784a
Dinar Fazliev <jjustdinar@gmail.com>
2023-10-25 13:19:58 +0300

    style(users.migrations) converted file to PEP8 standard.

--- users/migrations/0036_auto_20231023_1928.py
+++ users/migrations/0036_auto_20231023_1928.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations
 
+
 def migrate_data(apps, schema_editor):
 
     """
@@ -9,27 +10,28 @@ def migrate_data(apps, schema_editor):
         таблицы drevo.Suggestion и drevo.SuggestionType
     """
 
-        drevo_suggestion = apps.get_model('drevo', 'Suggestion')
-        users_suggestion = apps.get_model('users', 'UserSuggection')
-        drevo_type = apps.get_model('drevo', 'SuggestionType')
-        users_type = apps.get_model('users', 'SuggestionType')
+    drevo_suggestion = apps.get_model('drevo', 'Suggestion')
+    users_suggestion = apps.get_model('users', 'UserSuggection')
+    drevo_type = apps.get_model('drevo', 'SuggestionType')
+    users_type = apps.get_model('users', 'SuggestionType')
 
-        # Перенос данных из users.SuggestionType в drevo.SuggestionType
-        for i in users_type.objects.all(): 
-            drevo_type.objects.create(type_name = i.type_name, weight = i.type_name)
+    # Перенос данных из users.SuggestionType в drevo.SuggestionType
+    for i in users_type.objects.all():
+        drevo_type.objects.create(type_name=i.type_name, weight=i.type_name)
 
-        # Перенос данных из users.UserSuggestion в drevo.Suggestion
-        for i in users_suggestion.objects.all():
-            drevo_suggestion.objects.create(
-                parent_knowlege = i.parent_knowlege,
-                name = i.name,
-                user = i.user,
-                suggestion_type = drevo_type.objects.get(weight=i.suggestion_type.weight), # weight - поле уникальное
-                expert = i.expert,
-                is_approve = i.is_approve,
-                check_date = i.check_date,
-                create_date = i.create_date
-            )
+    # Перенос данных из users.UserSuggestion в drevo.Suggestion
+    for i in users_suggestion.objects.all():
+        drevo_suggestion.objects.create(
+            parent_knowlege=i.parent_knowlege,
+            name=i.name,
+            user=i.user,
+            suggestion_type=drevo_type.objects.get(weight=i.suggestion_type.weight),  # weight - поле уникальное
+            expert=i.expert,
+            is_approve=i.is_approve,
+            check_date=i.check_date,
+            create_date=i.create_date
+        )
+
 
 class Migration(migrations.Migration):
 
