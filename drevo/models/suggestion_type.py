3df5959ea21248f60481e86d5791f5cf02136092
Dinar Fazliev <jjustdinar@gmail.com>
2024-01-11 21:30:28 +0300

    fix (drevo.models) delete SuggestionType model

--- drevo/models/suggestion_type.py
+++ drevo/models/suggestion_type.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 
-class SuggestionType(models.Model):
-    type_name = models.CharField(max_length=255, verbose_name='Название типа')
-    weight = models.IntegerField(verbose_name='Порядок', default=100, blank=True)
-
-    def __str__(self):
-        return self.type_name
-
-    class Meta:
-        verbose_name = 'Вид предложения'
-        verbose_name_plural = 'Виды предложений'
-
-
 class SuggestionKind(models.Model):
     type_name = models.CharField(max_length=255, verbose_name='Название типа')
     weight = models.IntegerField(verbose_name='Порядок', default=100, blank=True)
