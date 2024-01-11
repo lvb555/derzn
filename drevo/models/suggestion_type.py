e107e8a754629ccb6e3fb0e2116986760211af9f
Dinar Fazliev <jjustdinar@gmail.com>
2024-01-11 20:56:27 +0300

    fix(drevo.models) create new model instead of SuggestionType
    Модель SuggestionType вызывает ошибку и необзодима в замене на модель SuggestionKind

--- drevo/models/suggestion_type.py
+++ drevo/models/suggestion_type.py
@@ -11,3 +11,15 @@ class SuggestionType(models.Model):
     class Meta:
         verbose_name = 'Вид предложения'
         verbose_name_plural = 'Виды предложений'
+
+
+class SuggestionKind(models.Model):
+    type_name = models.CharField(max_length=255, verbose_name='Название типа')
+    weight = models.IntegerField(verbose_name='Порядок', default=100, blank=True)
+
+    def __str__(self):
+        return self.type_name
+
+    class Meta:
+        verbose_name = 'Вид предложения'
+        verbose_name_plural = 'Виды предложений'
