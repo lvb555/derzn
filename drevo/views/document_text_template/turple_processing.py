edc2ed076506a6ea8b5a6687405378f3b3748c8c
Dinar Fazliev <jjustdinar@gmail.com>
2024-02-04 21:40:59 +0300

    (drevo.views) Привести к стандарту PEP8

--- drevo/views/document_text_template/turple_processing.py
+++ drevo/views/document_text_template/turple_processing.py
@@ -6,50 +6,52 @@ from django.urls import reverse
 from drevo.forms import TurpleForm
 from drevo.models import Turple, TurpleElement, Var
 
+
 def get_turple(pk):
     """
         Получить словарь с данным id
     """
     try:
         turple = Turple.objects.get(id=int(pk))
-    except Turple.DoesNotExist as e:
+    except Turple.DoesNotExist:
         raise Turple.DoesNotExist(json.dumps({
             'res': 'error',
             'error': f'Словаря с id {pk} не существует'}))
-    except ValueError as e:
+    except ValueError:
         raise ValueError(json.dumps({
-            'res': 'error', 
+            'res': 'error',
             'error': f'Не удалось распознать id {pk}'}))
 
     return turple
 
+
 def turple_processing_view(request, doc_pk):
     """
         Обработка запросов, касающихся справочников в сервисе создания шаблонов документов
-        GET: 
+        GET: запрос информации об словаре
+        POST: запрос на изменение/создание словаря
     """
     if request.method == 'POST':
-
+        # определить тип действия
         if 'id' in request.POST:
             action = 'edit'
             try:
                 turple = get_turple(request.POST["id"])
             except Exception as e:
-                return HttpResponse(str(e), content_type='application\json')
+                return HttpResponse(str(e), content_type='application/json')
         else:
             action = 'create'
 
         form = TurpleForm(request.POST)
 
         if form.is_valid():
-            new_turple = form.save(commit=(action=='create'))
+            new_turple = form.save(commit=(action == 'create'))
             if action == 'edit':
                 turple.name = new_turple.name
                 turple.is_global = new_turple.is_global
                 turple.weight = new_turple.weight
                 turple.save()
-
-
+            # список элементов справочника
             elements = zip(
                 request.POST.getlist('element-id'),
                 request.POST.getlist('element-weight'),
@@ -57,14 +59,14 @@ def turple_processing_view(request, doc_pk):
                 request.POST.getlist('element-name'))
             related_turple = (turple if action == 'edit' else new_turple)
             for pk, weight, var, value in elements:
-                if pk == '':
+                if pk == '':  # создание нового значения справочника
                     TurpleElement.objects.create(
                         value=value,
                         var=(None if var == '' else Var.objects.get(id=var)),
                         weight=weight,
                         turple=related_turple
                     )
-                else:
+                else:  # изменение существующего значения справочника
                     elem = TurpleElement.objects.get(id=int(pk))
                     elem.value = value
                     elem.weight = weight
@@ -74,24 +76,27 @@ def turple_processing_view(request, doc_pk):
 
             return HttpResponse(
                 json.dumps({
-                    'res': 'ok', 
-                    'turples': json.loads(serializers.serialize("json", Turple.objects.filter(knowledge=new_turple.knowledge)))}), 
-                content_type='application\json')
+                    'res': 'ok',
+                    'turples': json.loads(serializers.serialize("json", Turple.objects.filter(knowledge=new_turple.knowledge)))}),
+                content_type='application/json')
 
-        return HttpResponse(json.dumps({'res': 'validation error', 'errors': form.errors}), content_type='application\json')
+        return HttpResponse(
+            json.dumps({
+                'res': 'validation error',
+                'errors': form.errors}),
+            content_type='application/json')
     elif request.method == 'GET' and 'id' in request.GET:
-        
         try:
             turple = get_turple(request.GET["id"])
             turple_elements = TurpleElement.objects.filter(turple=turple)
         except Exception as e:
-            return HttpResponse(str(e), content_type='application\json')
+            return HttpResponse(str(e), content_type='application/json')
 
-        return HttpResponse(
+        return HttpResponse(  # вернуть информацию по требуемому справочнику
             json.dumps({
-                'res': 'ok', 
-                'turple': model_to_dict(turple), 
-                'elements': json.loads(serializers.serialize('json', turple_elements.all()))}), 
-            content_type='application\json')
+                'res': 'ok',
+                'turple': model_to_dict(turple),
+                'elements': json.loads(serializers.serialize('json', turple_elements.all()))}),
+            content_type='application/json')
     else:
         return HttpResponseRedirect(reverse('drevo'))
