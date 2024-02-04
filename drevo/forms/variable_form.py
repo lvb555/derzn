23e34cd10023d65e0c80fda10e7e2dff127bf506
Dinar Fazliev <jjustdinar@gmail.com>
2024-02-04 21:32:59 +0300

    (drevo.forms) Заменить логическое поле is_global на числовое поле availability, сделать поле availability обязательным к заполнению, заменить поле Структура на checkbox с наименование Массив, заменить наименования других полей, структуру Справочник сделать типом данных

--- drevo/forms/variable_form.py
+++ drevo/forms/variable_form.py
@@ -1,78 +1,79 @@
-from django.forms import ModelForm, RadioSelect, Textarea, TextInput, CheckboxInput, ModelChoiceField, NumberInput
+from django.forms import Textarea, NumberInput
 from django import forms
 from drevo.models import Var, Znanie, Turple
 from django.core.exceptions import ValidationError
 
 
-
 class VarForm(forms.Form):
+    """
+        Форма создания/редактирования объектов шаблона документа
 
-    available_sctructures = (('', 'Выберите структуру'), ) + Var.available_sctructures
-    available_types_of_content = (('', 'Выберите тип данных'), ) + Var.available_types_of_content
+        name - имя объекта
+        structure - структура объекта(для данной формы Переменная или Массив)
+        is_main - является ли объект главным в группе
+        availability - класс объекта(для данной формы Глобальный или Локальный)
+        subscription - нужна ли пропись
+        optional - является ли объект обязательным к заполнению
+        type_of - типы данных объекта
+        weight - вес, для сортировки
+        connected_to - родитель объекта
+        tuple - справочник всех допустимых значений, нужен, если тип данных Справочник
+        fill_title - тект, показываемый пользователю при инициализации объекта
+        knowledge - знание документа
+        action - вид действия(создание/изменение)
+        pk - редактирумей объект(нужен, если выбрано изменение)
+        comment - комментарий к объекту
+    """
+
+    available_sctructures = (('', 'Выберите структуру'), ) + Var.available_sctructures  # допустимые типы структрур объектов
+    available_types_of_content = (('', 'Выберите тип данных'), ) + Var.available_types_of_content  # допустимые типы содержимого
 
     def clean(self):
-
-        structure_list = ['Массив', 'Переменная', 'Справочник', 'Итератор']
-
         cleaned_data = super().clean()
 
         name = cleaned_data.get('name')
-        try:
-            structure = int(cleaned_data.get('structure'))
-        except:
-            structure = None
+        structure = cleaned_data.get('structure')
         is_main = cleaned_data.get('is_main')
-        is_global = cleaned_data.get('is_global')
+        availability = cleaned_data.get('availability')
         try:
             type_of = int(cleaned_data.get('type_of'))
         except:
             type_of = None
-        weight = cleaned_data.get('weight')
         connected_to = cleaned_data.get('connected_to')
-        turple = cleaned_data.get('turple')
-        fill_title = cleaned_data.get('fill_title')
         optional = cleaned_data.get('optional')
         subscription = cleaned_data.get('subscription')
         zn = cleaned_data.get('knowledge')
         var = cleaned_data.get('pk')
         action = cleaned_data.get('action')
-        comment = cleaned_data.get('comment')
 
-        # Проверка обязательных полей поля
-        l = [
-        (name, 'name'), 
-        (zn, 'knowledge'),
-        (is_main, 'is_main'), 
-        (optional, 'optional'),
-        (structure, 'structure'), 
-        (is_global, 'is_global'),
-        (subscription, 'subscription')]
+        # обязательные полей
+        required_fields = [
+            (name, 'name'),
+            (type_of, 'type'),
+            (zn, 'knowledge'),
+            (is_main, 'is_main'),
+            (optional, 'optional'),
+            (structure, 'structure'),
+            (availability, 'availability'),
+            (subscription, 'subscription')
+        ]
 
+        # Ясность действия
         if action not in ['edit', 'create']:
             raise ValidationError(f'Неопозднанный тип действия {action}')
 
-        """
-        Проверка на валидность введенных данных
-        """
-        
-        for v, n in l:
+        # Проверка на то, что все обязательные поля заполнены
+        for v, n in required_fields:
             if v is None:
                 raise ValidationError(f'Поле {n} должно быть заполнено')
 
+        # Указана ли редактируемая переменная
         if action == 'edit' and var is None:
-            raise ValidationError(f'Не задан редактируемый объект')
-
-        # проверка на то, что у структур Переменная, Массив указан тип содержимого
-        if structure <= 1 and type_of is None:
-            a = ['Переменная', 'Массив'][structure]
-            raise ValidationError(f'Поле type_of должно быть заполнено для объекта {a}')
+            raise ValidationError('Не задан редактируемый объект')
 
         # Проверка на то, что типы объектов и данных находятся в своих рамках
-        if type_of is not None and not (0 <= type_of < len(Var.available_types_of_content)):
+        if not (0 <= type_of < len(Var.available_types_of_content)):
             raise ValidationError(f'Нельзя привести число {type_of} к типу данных')
-
-        if not (0 <= structure < len(Var.available_sctructures)):
-            raise ValidationError(f'Нельзя привести число {structure} к типу объекта')
 
         # Проверка на уникальность имени
         count = Var.objects.filter(knowledge=zn, name=name).count()
@@ -82,32 +83,28 @@ class VarForm(forms.Form):
 
         # Проверка на совпадение типов структур главной и подчиняемой переменной
         if (not is_main) and (connected_to is not None) and (structure != connected_to.structure):
-            raise ValidationError(f'Объект типа {Var.available_sctructures[structure][1]} не может быть подчинен объекту типа {Var.available_sctructures[connected_to.structure][1]}')
-
+            raise ValidationError(f'Объект типа {Var.available_sctructures[int(structure)][1]} не может быть подчинен объекту типа {Var.available_sctructures[int(connected_to.structure)][1]}')
 
     name = forms.CharField(max_length=255, label='Имя объекта')
-    structure = forms.ChoiceField(
-        label='Структура',
-        choices=available_sctructures
-    )
-    is_main = forms.BooleanField(label='Главный', required=False)
-    is_global = forms.BooleanField(label='Глобальный', required=False)
+    structure = forms.BooleanField(label='Массив')
+    is_main = forms.BooleanField(label='Это группа', required=False)
+    availability = forms.BooleanField(label='Глобальный объект', required=False)
     subscription = forms.BooleanField(label='Прописью', required=False)
     optional = forms.BooleanField(label='Необязательность', required=False)
     type_of = forms.ChoiceField(
-        label='Тип',
+        label='Вид значения',
         choices=available_types_of_content,
         required=False)
     weight = forms.IntegerField(required=False, label='Порядок')
-    connected_to = forms.ModelChoiceField(queryset=Var.objects.all(), label='Подчинение', required=False, empty_label='Без подчинения')
+    connected_to = forms.ModelChoiceField(queryset=Var.objects.all(), label='Родитель', required=False, empty_label='Без подчинения')
     turple = forms.ModelChoiceField(queryset=Turple.objects.all(), label='Справочник', required=False, empty_label='Новый справочник')
     fill_title = forms.CharField(
-        label='Текст при инициализации',
+        label='Заголовок для окна диалога',
         required=False,
         widget=Textarea(attrs={
-            'cols': 10, 
-            'rows': 10, 
-            'class': 'form-control edit-menu__textarea',  
+            'cols': 10,
+            'rows': 10,
+            'class': 'form-control edit-menu__textarea',
             'id': 'fill-title'}))
     knowledge = forms.ModelChoiceField(queryset=Znanie.objects.all(), widget=NumberInput(attrs={'type': 'hidden'}))
     pk = forms.ModelChoiceField(queryset=Var.objects.all(), required=False)
