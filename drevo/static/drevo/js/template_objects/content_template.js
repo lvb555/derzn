23668cf82c41048c83c273138296b036e7b7a23f
Dinar Fazliev <jjustdinar@gmail.com>
2024-02-04 21:40:09 +0300

    (document text template page) Отобразить CKEditor, в обработке запросов создания/редактирования справочников и объектов заменить поле is_global на поле availability, реализовать вывод сообщений при создании/редактировании объектов

--- drevo/static/drevo/js/template_objects/content_template.js
+++ drevo/static/drevo/js/template_objects/content_template.js
@@ -1,8 +1,8 @@
 let edit_menu = document.querySelector(".edit-menu") // меню создания-редактирования переменной
-let action_type = document.querySelector(".edit-menu #action") // поле формы, отвечающее за тип действия (создать/редактировать)
-let message = document.querySelector(".edit-menu__log")
+let message_block = document.querySelector(".edit-menu__log-container")
 let editing_var = null // id редактируемой переменной
 let editing_turple = null // id редактируемого справочника
+let edit_menu_title = document.querySelector("#ObjectModal .modal-title")
 let action = null //
 let subscription_field = document.querySelector("#subscription") // чекбокс "прописью"
 let turple_field = document.querySelector("#turple") // поле выбора справочника
@@ -11,17 +11,17 @@ let main_checkbox = document.querySelector("#main input")
 let turple_menu_btns = document.querySelectorAll(".turple-selection-btns .turple-selection__btn")
 let url = window.location.href.split("document-template")[0] + "document-template"
 let csrftoken = getCookie("csrftoken")
-let types_block = document.querySelector("#type_of") // блок с полем выбора типа содержимого 
 let type = document.querySelector("#type_of select") // поле выбора типа содержимого
 let types = { // допустимые типы содержимого
 	"text": 0,
 	"number": 1,
-	"date": 2
+	"date": 2,
+	"tuple": 3
 }
 
-let structure = document.querySelector("#structure select") // поле выбора структуры объекта
+let structure = document.querySelector("#structure input") // поле выбора структуры объекта
 let structure_types = {} // допустимые типы содержимого
-new Array("var", "arr", "turple", "iterator", "if").forEach((i, index) => {
+new Array("var", "arr", "iterator", "if").forEach((i, index) => {
 	structure_types[i] = index
 })
 
@@ -30,8 +30,6 @@ function stripQueryParam(path) {
 	// обрезание параметров запроса из url адреса
 	return path.slice(0, path.indexOf("?"))
 }
-
-
 
 
 function TurpleEditID() {
@@ -53,7 +51,7 @@ function TurpleProcessingBody() {
 		body.append("id", editing_turple)
 	}
 	body.append("name", form.querySelector("#id_name").value)
-	body.append("is_global", form.querySelector("#id_is_global").checked)
+	body.append("availability", form.querySelector("#id_availability").checked)
 	body.append("weight", form.querySelector("#id_weight").value)
 	body.append("knowledge", form.querySelector("#id_knowledge").value)
 
@@ -127,47 +125,41 @@ function getCookie(name) {
 
 function edit_var_foo(e) {
 	// открыть форму для редактирования объекта
+	action = "edit"	
+	edit_menu_title = "Редактирование объекта шаблона"
+	editing_var = stripPrimaryKey(e.target.closest(".objects-list__object-card").id)
+	update_state(e)
 
-	if (!edit_menu.classList.contains("edit-menu_show")) {
-		message.innerHTML = ""
-		action = "edit"	
-		editing_var = stripPrimaryKey(e.target.closest(".objects-list__object-card").id)
+	fetch(url + `/document_object_processing?id=${editing_var}`, {"method": "get", "headers": {"X-CSRFToken": csrftoken}})
+	.then((response) => { return response.json() })
+	.then((ans) => {
+		if (ans["res"] !== "ok")
+			return
+		document.querySelectorAll(".edit-menu > .field input, .edit-menu > .field select").forEach((i) => {
+			if (i.type == "checkbox") {
+				i.checked = ans["object"][i.name]
+			} else if (i.type == "radio") {
+				i.checked = i.value == ans["object"][i.name]
+			} else {
+				i.value = ans["object"][i.name] !== null ? ans["object"][i.name] : ""
+			}
+		})
+		document.querySelector(".edit-menu textarea").value = ans["object"]["fill_title"]
+	})
+	.then(() => {
 		update_state(e)
-
-		fetch(url + `/document_object_processing?id=${editing_var}`, {"method": "get", "headers": {"X-CSRFToken": csrftoken}})
-		.then((response) => { return response.json() })
-		.then((ans) => {
-			if (ans["res"] !== "ok")
-				return
-			document.querySelectorAll(".edit-menu > .field input, .edit-menu > .field select").forEach((i) => {
-				if (i.type == "checkbox") {
-					i.checked = ans["object"][i.name]
-				} else if (i.type == "radio") {
-					i.checked = i.value == ans["object"][i.name]
-				} else {
-					i.value = ans["object"][i.name] !== null ? ans["object"][i.name] : ""
-				}
-			})
-			document.querySelector(".edit-menu textarea").value = ans["object"]["fill_title"]
-		})
-		.then(() => {
-			update_state(e)
-		})
-
-		edit_menu.classList.add("edit-menu_show")
-	}
+	})
 }
 
 
 function update_state(e) {
 	// обновить форму
-
-	is_turple_or_control = [structure_types["iterator"], structure_types["turple"], structure_types["if"]].includes(Number(structure.value))
-	subscription_field.style.display = type.value == types["text"] || is_turple_or_control ? "none" : "block"
-	types_block.style.display = is_turple_or_control ? "none" : "block"
-	connected_to_field.style.display = main_checkbox.checked || is_turple_or_control ? "none" : "block"
-	main_checkbox.parentElement.style.display = is_turple_or_control ? "none" : "block"
-	turple_field.style.display = structure.value == structure_types["turple"] ? "block" : "none"
+	console.log(types.value)
+	is_turple = type.value == types["tuple"]
+	subscription_field.style.display = type.value == types["text"] || is_turple ? "none" : "block"
+	// connected_to_field.style.display = main_checkbox.checked || is_turple ? "none" : "block"
+	// main_checkbox.parentElement.style.display = is_turple ? "none" : "block"
+	turple_field.style.display = type.value == types["tuple"] ? "block" : "none"
 	if (turple_field.querySelector("select").value === "") {
 		turple_menu_btns[1].style.display = "none"
 		turple_menu_btns[2].style.display = "none"
@@ -200,7 +192,7 @@ const csrftoken2 = getCookie('csrftoken')
 
 main_checkbox.addEventListener("change", update_state)
 
-document.querySelector("#structure select").addEventListener("change", update_state)
+document.querySelector("#structure input").addEventListener("change", update_state)
 
 
 // изменили тип значения
@@ -208,32 +200,20 @@ document.querySelector("#type_of select").addEventListener("change", update_stat
 
 
 // открыть форму для создания
-document.querySelector(".objects__create_btn").addEventListener("click", async (e) => {
-	if (!edit_menu.classList.contains("edit_menu_show")) {
+document.querySelector(".objects__create_btn").addEventListener("click", (e) => {
+	edit_menu_title.innerHTML = "Создание объекта шаблона"
+	action = "create"
+	editing_var = null
+	document.querySelectorAll(".edit-menu > .field input, .edit-menu > .field select").forEach((i) => {
+		if (i.type == "checkbox" || i.type == "radio") {
+			i.checked = false
+		} else {
+			i.value = ""
+		}
+	})
 
-		document.querySelectorAll(".edit-menu > .field input, .edit-menu > .field select").forEach((i) => {
-			if (i.type == "checkbox" || i.type == "radio") {
-				i.checked = false
-			} else {
-				i.value = ""
-			}
-		})
-
-		update_state(e)
-		edit_menu.classList.add("edit-menu_show")
-	}
+	update_state(e)
 })
-
-
-// открыть форму для создания
-document.querySelector(".objects__create_btn").addEventListener("click", async (e) => {
-	if (!edit_menu.classList.contains("edit_menu_show")) {
-		action = "create"
-		editing_var = null
-		message.innerHTML = ""
-	}
-})
-
 
 // селектор словаря
 document.querySelector(".turple-selection__field select").addEventListener("change", update_state)
@@ -254,6 +234,8 @@ document.querySelector(".edit-menu__save-btn").addEventListener("click", (e) => 
 	fetch(url + "/document_object_processing", {"method": "post", "body": body, "headers": {"X-CSRFToken": csrftoken}})
 	.then((response) => { return response.json() })
 	.then((ans) => {
+		let message = document.createElement("p")
+		message.classList.add("edit-menu__log")
 		console.log(ans)
 		if (ans["res"] == "ok") {
 			let blocks = document.querySelector(".objects__body")
@@ -275,9 +257,21 @@ document.querySelector(".edit-menu__save-btn").addEventListener("click", (e) => 
 				blocks.querySelector(`#structure-${i["fields"]["structure"]}`).appendChild(obj)
 			})
 
-		} else {
-			message.innerHTML = ans["errors"]["__all__"]
+			message.innerHTML = (action === "edit" ? "Изменения сохранены" : "Объект создан")
+
+		} else if (ans["res"] === "validation error") {
+			message.innerHTML = ans["errors"]["__all__"][0]
 		}
+		message_block.insertBefore(message, message_block.firstChild)
+		setTimeout(() => {
+			message.style.opacity = "100%"
+			setTimeout(() => {
+			message.style.opacity = "0%"
+			setTimeout(() => {
+				message.remove()
+			}, 510)
+		}, 1500)
+		}, 10)
 	})
 })
 
@@ -319,7 +313,7 @@ document.querySelector(".turple-selection__btn.create").addEventListener("click"
 	editing_turple = null
 	let form = document.querySelector(".turple-form")
 	form.querySelector("#id_name").value = ""
-	form.querySelector("#id_is_global").checked = false
+	form.querySelector("#id_availability").checked = false
 	form.querySelector("#id_weight").value = 100
 	let elements_block = document.querySelector(".turple-menu__objects-list")
 	while (elements_block.firstChild) {
@@ -332,7 +326,7 @@ document.querySelector(".turple-selection__btn.create").addEventListener("click"
 document.querySelector(".turple-selection__btn.edit").addEventListener("click", (e) => {
 	let form = document.querySelector(".turple-form")
 	form.querySelector("#id_name").value = ""
-	form.querySelector("#id_is_global").checked = false
+	form.querySelector("#id_availability").checked = false
 	form.querySelector("#id_weight").value = 100
 	let elements_block = document.querySelector(".turple-menu__objects-list")
 	while (elements_block.firstChild) {
@@ -347,7 +341,7 @@ document.querySelector(".turple-selection__btn.edit").addEventListener("click", 
 		console.log(ans)
 
 		form.querySelector("#id_name").value = ans["turple"]["name"]
-		form.querySelector("#id_is_global").checked = ans["turple"]["is_global"]
+		form.querySelector("#id_availability").checked = ans["turple"]["availability"]
 		form.querySelector("#id_weight").value = ans["turple"]["weight"]
 
 		
