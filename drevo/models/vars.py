0a2b9bf3e7b19a0776d6cbf5514b94e9a0df20b5
Dinar Fazliev <jjustdinar@gmail.com>
2024-02-17 21:59:33 +0300

    (drevo.models) Создать модель ObjectTemplate, наследуемый от MPTTModel(для работы с таблицей в формате деревьев), и провести миграцию данных из Var в ObjectTemplate

--- drevo/models/vars.py
+++ drevo/models/vars.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from django.db import models
+from mptt.managers import TreeManager
+from mptt.models import TreeForeignKey, MPTTModel
 
 
 class Var(models.Model):
@@ -76,3 +78,89 @@ class Var(models.Model):
         verbose_name = "Объект"
         verbose_name_plural = "Объекты шаблонов"
         ordering = ('weight',)
+
+
+class TemplateObject(MPTTModel):
+
+    """
+        объект в сервисе создания документов
+    """
+
+    available_types_of_content = (
+        (0, 'Текст'),
+        (1, 'Число'),
+        (2, 'Дата'),
+        (3, 'Справочник'),
+    )
+
+    available_sctructures = (
+        (0, 'Переменная'),
+        (1, 'Массив'),
+        (2, 'Управление')
+    )
+
+    types_of_availability = (
+        (0, 'Локальный'),
+        (1, 'Глобальный'),
+        (2, 'Общий')
+    )
+
+    name = models.CharField(max_length=50, verbose_name="Имя объекта")
+    structure = models.IntegerField(
+        default=0,
+        choices=available_sctructures,
+        verbose_name="Тип объекта")
+    is_main = models.BooleanField(default=False, verbose_name="Группа")
+    availability = models.IntegerField(
+        default=0,
+        choices=types_of_availability,
+        verbose_name="Класс объекта")
+    weight = models.IntegerField(default=1, verbose_name="Порядок")
+    fill_title = models.TextField(verbose_name="Обращение", default="", blank=True)
+    subscription = models.BooleanField(default=False, verbose_name="Прописью")
+    optional = models.BooleanField(default=False, verbose_name="Необязательность")
+    type_of = models.IntegerField(
+        default=0,
+        choices=available_types_of_content,
+        verbose_name="Вид значения")
+    knowledge = models.ForeignKey(
+        to="drevo.Znanie",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Знание")
+    connected_to = models.ForeignKey(
+        to='self',
+        on_delete=models.PROTECT,
+        verbose_name="Родитель",
+        null=True,
+        blank=True)
+    turple = models.ForeignKey(
+        to="drevo.Turple",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        verbose_name="Справочник")
+    comment = models.CharField(
+        max_length=255,
+        default='',
+        blank=True,
+        verbose_name="Комментарий")
+    # mptt_parent = TreeForeignKey(
+    #     'self',
+    #     on_delete=models.CASCADE,
+    #     null=True,
+    #     blank=True,
+    #     related_name='mptt_children'
+    #     )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Объект"
+        verbose_name_plural = "Объекты шаблонов"
+        ordering = ('weight',)
+
+    class MPTTMeta:
+        parent_attr = 'connected_to'
