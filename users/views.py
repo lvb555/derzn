47e3739a34315698bb39c01c4cd7ba7225b8bd9d
gReen7aRRow <f.gusarov@inbox.ru>
2024-02-25 18:38:10 +0300

    Joined profile_header to my_docs page

--- users/views.py
+++ users/views.py
@@ -255,13 +255,30 @@ class UserDocumentsView(LoginRequiredMixin, TemplateView):
     Описывает отображение шаблона "users/my_documents" по адресу "users/my_documents".
     """
 
-    template_name = "users/my_documents.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["documents"] = UsersDocuments.objects.filter(owner=self.request.user)
-
-        return context
+    def get(self, request):
+        if request.method == 'GET':
+            user = User.objects.filter(id=request.user.pk).first()
+            context = {}
+            if user is not None:
+                context["documents"] = UsersDocuments.objects.filter(owner=self.request.user)
+                if user == request.user:
+                    context['sections'] = access_sections(user)
+                    context['activity'] = [i for i in context['sections'] if i.startswith('Мои') or
+                                        i.startswith('Моя')]
+                    context['link'] = 'users:myprofile'
+                    invite_count = FriendsInviteTerm.objects.filter(recipient=request.user.id).count()
+                    context['invite_count'] = invite_count if invite_count else 0
+                    context['new_knowledge_feed'] = FeedMessage.objects.filter(recipient=user, was_read=False).count()
+                    context['new_messages'] = Message.objects.filter(recipient=user, was_read=False).count()
+                    context['new'] = int(context['new_knowledge_feed']) + int(
+                        context['invite_count'] + int(context['new_messages']))
+                else:
+                    context['sections'] = [i.name for i in user.sections.all()]
+                    context['activity'] = [i.name for i in user.sections.all() if i.name.startswith('Мои') or i.name.startswith('Моя')]
+                    context['link'] = 'public_human'
+                    context['id'] = id
+                context['pub_user'] = user
+                return render(request, 'users/my_documents.html', context)
 
 
 class UserVerifyView(TemplateView):
