8a97904a0982b597c0dea581a8ef525dcadb6fd0
Akilo <sergeencowgame@yandex.ru>
2024-02-29 18:10:11 +0300

    Создание страницы сотрудников

--- drevo/urls.py
+++ drevo/urls.py
@@ -180,6 +180,7 @@ from .views.knowledge_tp_view import (
     DirectorKnowledgeProcess,
     KlzKnowledgeProcess,
 )
+from .views.editorial_staff import editorial_staff_view, update_roles
 
 urlpatterns = [
     path("category/<int:pk>", DrevoListView.as_view(), name="drevo_type"),
@@ -603,6 +604,8 @@ urlpatterns = [
          name="delete_algorithm"),
     path("delete_zn_in_cell_in_table/", delete_zn_in_cell_in_table,
          name="delete_zn_in_cell_in_table"),
+    path("editorial_staff/", editorial_staff_view, name='editorial_staff'),
+    path('editorial_staff/update_roles/', update_roles, name='update_roles'),
 
 ]
 
