b3846640fecc071fc9ea0e583c270b3ccc201406
RomanBorovskiy <borovskiyroman@gmail.com>
2024-03-06 16:02:18 +0300

    форматирование black

--- drevo/models/knowledge_kind.py
+++ drevo/models/knowledge_kind.py
@@ -14,61 +14,49 @@ class Tz(models.Model):
     title = "Вид знания"
     name = models.CharField(max_length=128, unique=True, verbose_name="Название")
     order = models.PositiveSmallIntegerField(
-        verbose_name='Порядок',
-        help_text='укажите порядковый номер',
+        verbose_name="Порядок",
+        help_text="укажите порядковый номер",
         default=0,
-        blank=True
+        blank=True,
     )
-    is_systemic = models.BooleanField(
-        default=False,
-        verbose_name='Системный?'
-    )
+    is_systemic = models.BooleanField(default=False, verbose_name="Системный?")
 
-    is_group = models.BooleanField(
-        default=False,
-        verbose_name='Вид является группой?'
-    )
+    is_group = models.BooleanField(default=False, verbose_name="Вид является группой?")
 
     can_be_rated = models.BooleanField(
-        default=False,
-        verbose_name='Возможна оценка знания'
+        default=False, verbose_name="Возможна оценка знания"
     )
-    is_send = models.BooleanField(
-        default=True,
-        verbose_name='Пересылать'
-    )
+    is_send = models.BooleanField(default=True, verbose_name="Пересылать")
     is_author_required = models.BooleanField(
-        default=False,
-        verbose_name='Автор обязателен для заполнения'
+        default=False, verbose_name="Автор обязателен для заполнения"
     )
     is_href_required = models.BooleanField(
-        default=False,
-        verbose_name='Источник обязателен для заполнения'
+        default=False, verbose_name="Источник обязателен для заполнения"
     )
     min_number_of_inner_rels = models.PositiveSmallIntegerField(
-        default=0,
-        verbose_name='Минимальное число внутренних связей'
+        default=0, verbose_name="Минимальное число внутренних связей"
     )
     max_number_of_inner_rels = models.PositiveSmallIntegerField(
-        default=0,
-        verbose_name='Максимальное число внутренних связей'
+        default=0, verbose_name="Максимальное число внутренних связей"
     )
     objects = models.Manager()
 
-    available_suggestion_types = models.ManyToManyField(to='drevo.SuggestionType',
-        verbose_name='Виды предложений',
-        blank=True)
+    available_suggestion_types = models.ManyToManyField(
+        to="drevo.SuggestionType", verbose_name="Виды предложений", blank=True
+    )
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
         if self.max_number_of_inner_rels < self.min_number_of_inner_rels:
-            raise ValidationError("Максимальное кол-во внутренних связей не может быть меньше минимального!")
+            raise ValidationError(
+                "Максимальное кол-во внутренних связей не может быть меньше минимального!"
+            )
         znaniya = Znanie.objects.filter(tz=self).all()
         for znanie in znaniya:
             znanie.is_send = self.is_send
-        Znanie.objects.bulk_update(znaniya, ['is_send'])
+        Znanie.objects.bulk_update(znaniya, ["is_send"])
         super(Tz, self).save(*args, **kwargs)
 
     def sorted_suggestion_types(self):
@@ -101,6 +89,6 @@ class Tz(models.Model):
                 raise ValueError(f"Не найден тип знаний: {item}")
 
     class Meta:
-        verbose_name = 'Вид знания'
-        verbose_name_plural = 'Виды знания'
-        ordering = ('order',)
+        verbose_name = "Вид знания"
+        verbose_name_plural = "Виды знания"
+        ordering = ("order",)
--- drevo/models/relation_type.py
+++ drevo/models/relation_type.py
@@ -12,37 +12,34 @@ class Tr(models.Model):
     AGAINST = True
 
     ARGUMENT_TYPES = (
-        (FOR, 'За'),
-        (AGAINST, 'Против'),
+        (FOR, "За"),
+        (AGAINST, "Против"),
     )
 
-    title = 'Вид связи'
-    name = models.CharField(max_length=255,
-                            verbose_name='Название',
-                            unique=True
-                            )
+    title = "Вид связи"
+    name = models.CharField(max_length=255, verbose_name="Название", unique=True)
     order = models.PositiveSmallIntegerField(
-        verbose_name='Порядок',
-        help_text='укажите порядковый номер',
+        verbose_name="Порядок",
+        help_text="укажите порядковый номер",
         default=0,
     )
-    is_systemic = models.BooleanField(default=False,
-                                      verbose_name='Системный?')
-    is_argument = models.BooleanField(default=False,
-                                      verbose_name='Доказательная связь')
-    argument_type = models.BooleanField(choices=ARGUMENT_TYPES,
-                                        default=FOR,
-                                        verbose_name='Тип довода')
-    has_invert = models.BooleanField(editable=False,
-                                     default=False,
-                                     verbose_name='Доступна инверсия')
-    invert_tr = models.ForeignKey('self',
-                                  blank=True,
-                                  null=True,
-                                  on_delete=models.CASCADE,
-                                  related_name='invert_rel_type',
-                                  verbose_name='Инверсия',
-                                  help_text='Только для инверсивных связей')
+    is_systemic = models.BooleanField(default=False, verbose_name="Системный?")
+    is_argument = models.BooleanField(default=False, verbose_name="Доказательная связь")
+    argument_type = models.BooleanField(
+        choices=ARGUMENT_TYPES, default=FOR, verbose_name="Тип довода"
+    )
+    has_invert = models.BooleanField(
+        editable=False, default=False, verbose_name="Доступна инверсия"
+    )
+    invert_tr = models.ForeignKey(
+        "self",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="invert_rel_type",
+        verbose_name="Инверсия",
+        help_text="Только для инверсивных связей",
+    )
 
     objects = models.Manager()
 
@@ -79,9 +76,12 @@ class Tr(models.Model):
                 raise ValueError(f"Не найден тип связи: {item}")
 
     class Meta:
-        verbose_name = 'Вид связи'
-        verbose_name_plural = 'Виды связи'
-        ordering = ('order', 'name',)
+        verbose_name = "Вид связи"
+        verbose_name_plural = "Виды связи"
+        ordering = (
+            "order",
+            "name",
+        )
 
     def __str__(self):
         return self.name
