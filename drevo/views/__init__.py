15cc037a3d6c68dded8b17c967fd53083044e930
Dinar Fazliev <jjustdinar@gmail.com>
2024-03-11 12:22:56 +0300

    Импорт представлений из папки drevo/views/document_text_template в __init__ файл этого пути

--- drevo/views/__init__.py
+++ drevo/views/__init__.py
@@ -140,10 +140,13 @@ from .relations_preparing_work import (
     relation_publication_view,
 )
 
-from .document_text_template.document_text_template_edit import DocumentTextTemplateEdit
-from .document_text_template.turple_processing import turple_processing_view
-from .document_text_template.object_processing import document_object_processing_view
-from .document_text_template.objects_tree import ObjectsTree
+from .document_text_template import (
+    DocumentTextTemplateEdit,
+    turple_processing_view,
+    document_object_processing_view,
+    ObjectsTree,
+    save_text_template_view
+)
 
 __all__ = [
     "AuthorDetailView",
