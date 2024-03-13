09ba0483ae8bf4855ffe6e9b40cd682fb18b057d
RomanBorovskiy <borovskiyroman@gmail.com>
2024-03-13 10:25:25 +0300

    изменены пути urls

--- drevo/urls.py
+++ drevo/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 
 from drevo.views.interviews_view import interview_view
 from drevo.views.my_interview_view import my_interview_view
+from drevo.views.zn_constructors.table_constructor_view import TableConstructView, TableFillingView
+from drevo.views.knowledge_view import KnowledgeCreateView as KnowledgeCreateViewModal
 from .views import (
     DrevoListView,
     DrevoView,
@@ -33,7 +35,6 @@ from .views import (
     friends_added_view,
     ZnaniyaForConstructorView,
     MainZnInConstructorCreateView,
-    FillingTablesView,
     save_zn_to_cell_in_table_from_request,
     get_cell_for_table,
     relation_in_table_create_update_view,
@@ -103,7 +104,6 @@ from .views import (
     get_tr_for_create_relation_in_tree_constructor,
     get_rel_zn_in_tree_constructor_from_request,
     TreeConstructorView,
-    TableConstructorView,
     save_rel_in_tree_constructor,
     make_copy_of_algorithm,
     delete_relation_in_tree_constructor,
@@ -120,8 +120,6 @@ from .views import (
     delete_zn_in_cell_in_table,
 
 )
-from .views.zn_constructors.table_constructor_view import TableFillingView, TableConstructView
-from drevo.views.knowledge_view import KnowledgeCreateView as KnowledgeCreateView2
 
 from .views import send_znanie, knowledge_feed_view
 from .views.appeal_in_support import appeal
@@ -192,7 +190,8 @@ urlpatterns = [
     path("znanie/<int:pk>", ZnanieDetailView.as_view(), name="zdetail"),
     path("znanie/<int:pk>/suggestions", UserSuggestionView.as_view(), name="create-suggestion"),
     path("znanie/<int:pk>/questions_user", save_answer, name="questions_user"),
-    path("znanie/<int:pk>/questions_and_check_answers", questions_and_check_answers, name="questions_and_check_answers"),
+    path("znanie/<int:pk>/questions_and_check_answers", questions_and_check_answers,
+         name="questions_and_check_answers"),
     path("znanie/<int:pk>/favourite", FavouriteProcessView.as_view()),
     path("znanie/<int:pk>/comments/", CommentPageView.as_view()),
     path("znanie/<int:pk>/comments/send/", CommentSendView.as_view()),
@@ -204,13 +203,16 @@ urlpatterns = [
     path('znanie/<int:pk>/grade/statistic', KnowledgeStatisticFormView.as_view(), name='grade_statistic'),
     path('znanie/<int:pk>/grade/group', GroupKnowledgeView.as_view(), name="group_knowledge"),
     path('znanie/<int:pk>/grade/group/infographics', GroupInfographicsView.as_view(), name="grade_group_infographics"),
-    path('znanie/<int:pk>/grade/group/statistics', GroupKnowledgeStatisticsView.as_view(), name="grade_group_statistics"),
+    path('znanie/<int:pk>/grade/group/statistics', GroupKnowledgeStatisticsView.as_view(),
+         name="grade_group_statistics"),
     path("znanie/<int:pk>/grade/infographics", InfographicsView.as_view(), name="grade_infographics"),
-    path('znanie/<int:doc_pk>/document-template/edit-text/<int:text_pk>', DocumentTextTemplateEdit.as_view(), name='edit_text_template'),
-    path('znanie/<int:doc_pk>/document-template/create-text/', DocumentTextTemplateCreate.as_view(), name='create_text_template'),
+    path('znanie/<int:doc_pk>/document-template/edit-text/<int:text_pk>', DocumentTextTemplateEdit.as_view(),
+         name='edit_text_template'),
     path('znanie/<int:doc_pk>/document-template/turple_processing', turple_processing_view),
     path('znanie/<int:doc_pk>/document-template/document_object_processing', document_object_processing_view),
-    path('znanie/list/', KnowledgeListView.as_view(), name='get_knowledge_list'),
+    path('znanie/<int:doc_pk>/document-template/object-select', ObjectsTree.as_view(), name='select_object_from_tree'),
+    path('znanie/<int:doc_pk>/document-template/save-text-template', save_text_template_view,
+         name='save_text_template'),
     path("knowledges_grades/", knowledges_grades, name="knowledges_grades"),
     path("my_knowledge_grade/<int:id>/", my_knowledge_grade, name="my_knowledge_grade"),
     path("about/", about_proj.AboutView.as_view(), name="about_proj"),
@@ -422,8 +424,8 @@ urlpatterns = [
         knowledge_feed_view.delete_message,
         name="delete_feed_message",
     ),
-    path("send-message/", send_message_view.send_message, name = "send_message"),
-    path("messages-feed/", messages_feed_view.messages_feed, name = "messages_feed"),
+    path("send-message/", send_message_view.send_message, name="send_message"),
+    path("messages-feed/", messages_feed_view.messages_feed, name="messages_feed"),
     path(
         "messages-feed/delete/<int:message_id>/",
         send_message_view.delete_message,
@@ -550,14 +552,9 @@ urlpatterns = [
         name='relation_publication'
     ),
 
-    # конструирование и наполнение таблиц
-    path("table_constructor/<int:pk>/", TableConstructView.as_view(), name="table_constructor"),
-    path("filling_tables/<int:pk>/", TableFillingView.as_view(), name="filling_tables"),
-    # для вызова модального окна
-    path("znanie/add/", KnowledgeCreateView2.as_view(), name="knowledge_create"),
-
-
-    path("save_zn_to_cell_in_table_from_request/", save_zn_to_cell_in_table_from_request, name="save_zn_to_cell_in_table_from_request"),
+    # ---------------------------------------------------------------------------------------
+    path("save_zn_to_cell_in_table_from_request/", save_zn_to_cell_in_table_from_request,
+         name="save_zn_to_cell_in_table_from_request"),
     path("get_cell_for_table/", get_cell_for_table, name="get_cell_for_table"),
     path(
         'main_znanie_in_constructor_create/<type_of_zn>/',
@@ -565,7 +562,6 @@ urlpatterns = [
         name="main_znanie_in_constructor_create"
     ),
     path('znaniya_for_constructor/', ZnaniyaForConstructorView.as_view(), name="znaniya_for_constructor"),
-
     path("delete_table/", delete_table, name="delete_table"),
     path("delete_row_or_column/", delete_row_or_column, name="delete_row_or_column"),
     path("show_filling_tables_page/", show_filling_tables_page, name="show_filling_tables_page"),
@@ -583,13 +579,23 @@ urlpatterns = [
         get_tr_for_create_relation_in_tree_constructor,
         name="rel_in_tree_constructor_create"
     ),
-    path("get_rel_zn_in_tree_constructor_from_request/", get_rel_zn_in_tree_constructor_from_request, name="get_rel_zn_in_tree_constructor_from_request"),
+
+    # ---------------------------------------------------------------------------------------
+    # конструирование и наполнение таблиц
+    path("table_constructor/<int:pk>/", TableConstructView.as_view(), name="table_constructor"),
+    path("filling_tables/<int:pk>/", TableFillingView.as_view(), name="filling_tables"),
+    # для вызова модального окна
+    path("znanie/add/", KnowledgeCreateViewModal.as_view(), name="knowledge_create"),
+
+    path("get_rel_zn_in_tree_constructor_from_request/", get_rel_zn_in_tree_constructor_from_request,
+         name="get_rel_zn_in_tree_constructor_from_request"),
     path("quiz_constructor/<int:pk>/", QuizConstructorView.as_view(), name="quiz_constructor"),
     path("tree_constructor/<type>/<int:pk>/", TreeConstructorView.as_view(), name="tree_constructor"),
 
     path("save_rel_in_tree_constructor/", save_rel_in_tree_constructor, name="save_rel_in_tree_constructor"),
     path("make_copy_of_algorithm/", make_copy_of_algorithm, name="make_copy_of_algorithm"),
-    path("delete_relation_in_tree_constructor/", delete_relation_in_tree_constructor, name="delete_relation_in_tree_constructor"),
+    path("delete_relation_in_tree_constructor/", delete_relation_in_tree_constructor,
+         name="delete_relation_in_tree_constructor"),
     path("rel_in_tree_constructor/edit/", get_tr_for_edit_relation_in_tree_constructor,
          name="rel_in_tree_constructor_edit"),
     path("edit_znanie_in_tree_constructor/", edit_znanie_in_tree_constructor,
@@ -616,12 +622,12 @@ urlpatterns = [
          name="delete_algorithm"),
     path("delete_zn_in_cell_in_table/", delete_zn_in_cell_in_table,
          name="delete_zn_in_cell_in_table"),
+    # ---------------------------------------------------------------------------------------
+
     path("editorial_staff/", editorial_staff_view, name='editorial_staff'),
     path('editorial_staff/update_roles/', update_roles, name='update_roles'),
 
 ]
-
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
