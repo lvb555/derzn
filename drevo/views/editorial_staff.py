2be488b46c462ae3752743b64434e9b0d1f3a543
Akilo <sergeencowgame@yandex.ru>
2024-04-02 22:19:49 +0300

    Немного привел код в порядок, также изменил направление отчества на новое.

--- drevo/views/editorial_staff.py
+++ drevo/views/editorial_staff.py
@@ -30,67 +30,65 @@ def editorial_staff_view(request):
 
 @login_required
 def update_roles(request):
-    if request.user.is_superuser:
-        if request.method == 'POST':
-            user_id = request.POST.get('userId')
-            is_employee = request.POST.get('isEmployee') == 'true'
-            is_admin = request.POST.get('isAdmin') == 'true'
-
-            try:
-                user = User.objects.get(id=user_id)
-                message = f'Уважаемый {user.first_name} {user.profile.patronymic}! \n'
-
-                if is_employee and is_admin:
-                    user.is_employee = True
-                    user.is_superuser = True
-                    message += "Вам дано право Сотрудника редакции и Администратора портала."
-                    subject = "Дано право Сотрудника редакции и Администратора портала"
-                elif is_employee:
-                    user.is_employee = True
-                    user.is_superuser = False
-                    message += "Вам дано право Сотрудника редакции."
-                    subject = "Дано право Сотрудника редакции"
-                elif is_admin:
-                    user.is_superuser = True
-                    message += "Вам дано право Администратора портала."
-                    subject = "Дано право Администратора портала"
-                else:
-                    user.is_employee = False
-                    user.is_superuser = False
-                    message = "Вы лишены всех прав."
-                    subject = "Лишение всех прав пользователя"
-
-                message += '\n\nРедакция портала "Дерево знаний" '
-                user.save()
-
-                send_email(user.email, subject, False, message)
-
-                return JsonResponse({'message': 'Roles updated successfully'})
-
-            except User.DoesNotExist:
-                return JsonResponse({'error': 'User does not exist'}, status=404)
-
-            except Exception as e:
-                return JsonResponse({'error': str(e)}, status=500)
+    if not request.user.is_superuser or not request.method == 'POST':
         return JsonResponse({'error': 'Invalid request'}, status=400)
+
+    user_id = request.POST.get('userId')
+    is_employee = request.POST.get('isEmployee') == 'true'
+    is_admin = request.POST.get('isAdmin') == 'true'
+
+    user = User.objects.get(id=user_id)
+    message = f'Уважаемый {user.first_name} {user.profile.patronymic}! \n'
+
+    if is_employee and is_admin:
+        user.is_employee = True
+        user.is_superuser = True
+        message += "Вам дано право Сотрудника редакции и Администратора портала."
+        subject = "Дано право Сотрудника редакции и Администратора портала"
+    elif is_employee:
+        user.is_employee = True
+        user.is_superuser = False
+        message += "Вам дано право Сотрудника редакции."
+        subject = "Дано право Сотрудника редакции"
+    elif is_admin:
+        user.is_superuser = True
+        message += "Вам дано право Администратора портала."
+        subject = "Дано право Администратора портала"
+    else:
+        user.is_employee = False
+        user.is_superuser = False
+        message = "Вы лишены всех прав."
+        subject = "Лишение всех прав пользователя"
+
+    message += '\n\nРедакция портала "Дерево знаний" '
+    user.save()
+
+    send_email(user.email, subject, False, message)
+
+    return JsonResponse({'message': 'Roles updated successfully'})
+
+
+
 
 
 @login_required
 def update_user_permissions(request):
-    if request.user.is_superuser:
-        if request.method == 'POST':
-            data = json.loads(request.body)
+    if not request.user.is_superuser or not request.method == 'POST':
+        return JsonResponse({'error': 'Invalid request'}, status=400)
+    data = json.loads(request.body)
 
-            user_id = data.get('userId')
-            group = Group.objects.get(name=data.get('group'))
-            granted = data.get('granted')
+    user_id = data.get('userId')
+    group = Group.objects.get(name=data.get('group'))
+    granted = data.get('granted')
 
-            user = User.objects.get(id=user_id)
-            permissions = Permission.objects.filter(group=group)
+    user = User.objects.get(id=user_id)
+    permissions = Permission.objects.filter(group=group)
 
-            if granted:
-                user.groups.add(group)
-                user.user_permissions.add(*permissions)
-            else:
-                user.groups.remove(group)
-                user.user_permissions.remove(*permissions)
+    if granted:
+        user.groups.add(group)
+        user.user_permissions.add(*permissions)
+    else:
+        user.groups.remove(group)
+        user.user_permissions.remove(*permissions)
+    return JsonResponse({'message': 'permissions updated successfully'})
+
