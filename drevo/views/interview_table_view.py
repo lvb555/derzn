45e38a87de99c6a4b44b725322409216351f593a
Suleiman <Suleiman.turgunbaev@bk.ru>
2024-04-23 16:27:18 +1200

    Solution

--- drevo/views/interview_table_view.py
+++ drevo/views/interview_table_view.py
@@ -5,24 +5,24 @@ from drevo.models.relation import Relation
 
 def interview_table(request, id):
     interview = get_object_or_404(Znanie, id=id)
-    interview_this = interview
     questions = Relation.objects.filter(tr__name="Состав", bz__id=interview.id)
     question_list = [question.rz for question in questions]
     authors_dict = defaultdict(lambda: defaultdict(list))
     author_names = defaultdict(str)
     answers = Relation.objects.filter(tr__name="Ответ", bz__in=question_list).select_related('rz__author').all()
-    for question in questions:
-        for answer in answers:
-            if answer.bz.id == question.rz.id:
-                author = answer.rz.user
-                authors_dict[author.id][question.rz].append(answer.rz.name)
-                if answer.rz.user.patronymic:
-                    short_fst_name = answer.rz.user.first_name[0]
-                    short_patr = answer.rz.user.patronymic[0]
-                    author_names[author.id] = f"{short_fst_name}.{short_patr}.{answer.rz.user.last_name}"
-                else:
-                    short_fst_name = author.first_name[0]
-                    author_names[author.id] = f"{short_fst_name}.{answer.rz.user.last_name}"
+
+    for answer in answers:
+        question = answer.bz
+        author = answer.rz.user
+        authors_dict[author.id][question].append(answer.rz.name)
+        if author_names[author.id] == "":
+            if answer.rz.user.patronymic:
+                short_fst_name = answer.rz.user.first_name[0]
+                short_patr = answer.rz.user.patronymic[0]
+                author_names[author.id] = f"{short_fst_name}.{short_patr}.{answer.rz.user.last_name}"
+            else:
+                short_fst_name = author.first_name[0]
+                author_names[author.id] = f"{short_fst_name}.{answer.rz.user.last_name}"
 
     table = [[""] + question_list]
     for author_id, answers in authors_dict.items():
@@ -36,5 +36,5 @@ def interview_table(request, id):
             table.append(row)
 
     return render(request, "drevo/interview_table.html", {
-        'table': table, 'interview_this': interview_this
+        'table': table, 'interview': interview
     })
