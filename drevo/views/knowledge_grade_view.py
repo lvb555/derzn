f6e4246263c0dc717c098d81d5457620f76dba4b
RomanBorovskiy <borovskiyroman@gmail.com>
2024-06-03 17:23:48 +0300

    форматирование

--- drevo/views/knowledge_grade_view.py
+++ drevo/views/knowledge_grade_view.py
@@ -8,17 +8,19 @@ from drevo.models.knowledge_grade_scale import KnowledgeGradeScale
 from drevo.models.relation import Relation
 from drevo.models.relation_grade import RelationGrade
 from drevo.models.relation_grade_scale import RelationGradeScale
-from drevo.utils.knowledge_grader import KnowledgeGraderService
 from drevo.utils.common import validate_parameter_int
+from drevo.utils.knowledge_grader import KnowledgeGraderService
 
 
 class KnowledgeFormView(LoginRequiredMixin, TemplateView):
     template_name = "drevo/knowledge_grade/knowledge_grade.html"
-    partial_name = 'drevo/knowledge_grade/score_card.html'
+    partial_name = "drevo/knowledge_grade/score_card.html"
 
     def get_context_data(self, **kwargs):
         user = self.request.user
-        knowledge = get_object_or_404(Znanie.objects.select_related('tz'), id=kwargs.get("pk"))
+        knowledge = get_object_or_404(
+            Znanie.objects.select_related("tz"), id=kwargs.get("pk")
+        )
         # только для знаний, которые можно оценить
         if not knowledge.tz.can_be_rated:
             raise Http404
@@ -38,7 +40,9 @@ class KnowledgeFormView(LoginRequiredMixin, TemplateView):
         context["knowledge_scale"] = KnowledgeGradeScale.get_cache()
         context["relation_scale"] = RelationGradeScale.get_cache()
 
-        variant = validate_parameter_int(self.request.GET.get("variant"), default=1, good_values=[1, 2])
+        variant = validate_parameter_int(
+            self.request.GET.get("variant"), default=1, good_values=[1, 2]
+        )
         context["variant"] = variant
 
         grader = KnowledgeGraderService(user, knowledge)
@@ -50,28 +54,32 @@ class KnowledgeFormView(LoginRequiredMixin, TemplateView):
         return context
 
     def post(self, request, *args, **kwargs):
-        """ Возвращает только частичный рендер страницы с оценками
-            для отображения с помощью htmx
+        """Возвращает только частичный рендер страницы с оценками
+        для отображения с помощью htmx
         """
         user = request.user
 
-        new_knowledge_grade = request.POST.get('knowledge_grade', None)
-        new_relation_grade = request.POST.get('relation_grade', None)
-        proof_relation = request.POST.get('relation', None)
-        proof_knowledge = request.POST.get('knowledge', None)
+        new_knowledge_grade = request.POST.get("knowledge_grade", None)
+        new_relation_grade = request.POST.get("relation_grade", None)
+        proof_relation = request.POST.get("relation", None)
+        proof_knowledge = request.POST.get("knowledge", None)
 
-        if (new_knowledge_grade and
-                proof_knowledge and
-                new_knowledge_grade != KnowledgeGradeScale.get_default_grade().id):
+        if (
+            new_knowledge_grade
+            and proof_knowledge
+            and new_knowledge_grade != KnowledgeGradeScale.get_default_grade().id
+        ):
             KnowledgeGrade.objects.update_or_create(
                 knowledge_id=proof_knowledge,
                 user=user,
                 defaults={"grade_id": new_knowledge_grade},
             )
 
-        if (new_relation_grade and
-                proof_relation and
-                new_relation_grade != RelationGradeScale.get_default_grade().id):
+        if (
+            new_relation_grade
+            and proof_relation
+            and new_relation_grade != RelationGradeScale.get_default_grade().id
+        ):
             RelationGrade.objects.update_or_create(
                 relation_id=proof_relation,
                 user=user,
