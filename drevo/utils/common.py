cf81ba593c5d7ca7a2d13f4085625721dbe80a30
RomanBorovskiy <borovskiyroman@gmail.com>
2024-06-15 17:00:10 +0300

    По умолчанию вариант расчета берется из настройки пользователя с идентификатором SCORE_VARIANT

--- drevo/utils/common.py
+++ drevo/utils/common.py
@@ -1,3 +1,7 @@
+from drevo.models import UserParameters, SettingsOptions
+from users.models import User
+
+
 def validate_parameter_int(param: str | int | None, default: int = 0, good_values: list[int] | None = None) -> int:
     """ Валидация параметра param, который может быть числом, строкой или None
         Если передан список good_values - проверяется на вхождение в него
@@ -18,3 +22,23 @@ def validate_parameter_int(param: str | int | None, default: int = 0, good_value
         return default
 
     return param
+
+
+def get_user_parameter(user: User, parameter_id: int) -> int | None:
+    """ Ищет значение параметра пользователя по названию параметра
+        Если такого параметра вообще нет - возвращает None
+        Если у пользователя есть такой параметр - возвращает его значение
+        Если у пользователя такого параметра нет (такого не может быть) - возвращает значение по умолчанию для параметра
+    """
+    # ищем по названию параметра
+    option = SettingsOptions.objects.filter(pk=parameter_id).first()
+    # если не находим - значит параметра такого нет
+    if not option:
+        return None
+
+    param = UserParameters.objects.filter(user=user, param=option).first()
+    if param:
+        return param.param_value
+    else:
+        return option.default_param
+
