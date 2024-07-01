7758b8da038421e38794433ec2c0cca5c0ed3c99
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-01 15:02:29 +0300

    Рефакторинг кода обновления формы редакторивания объекта и выбора объекта

--- drevo/static/drevo/js/template_objects/objects_tree.js
+++ drevo/static/drevo/js/template_objects/objects_tree.js
@@ -1,3 +1,5 @@
+// Код для страницы drevo/znanie/<int:doc_pk>/document-template/edit-text/<int:text_pk>
+// не затрагивающий HTTP-запросы
 
 const url = window.location.href.split("document-template")[0] + "document-template"
 const message_block = document.querySelector(".log-container")
@@ -5,12 +7,33 @@ const expand_children = document.querySelectorAll(".node__expand-btn")
 const collapse_children = document.querySelectorAll(".node__collapse-btn")
 const objects_contaning_list = document.querySelector(".objects-tree__containing-list")
 
-function select_obejct(e) {
+const turple_block = document.querySelector("#tuple") // поле выбора справочника
+
+const subscription_block = document.querySelector("#subscription") // чекбокс "прописью"
+
+const type = document.querySelector("#type_of select") // поле выбора типа содержимого
+const types = { // допустимые типы содержимого
+	"text": 0,
+	"number": 1,
+	"date": 2,
+	"tuple": 3
+}
+
+export let deleting_object = null
+export let action = null
+export let editing_var = null
+
+function update_state(e) {
+	// обновить форму
+	let is_turple = type.value == types["tuple"]
+	subscription_block.style.display = type.value == types["text"] || is_turple ? "none" : "block"
+	turple_block.style.display = type.value == types["tuple"] ? "block" : "none"
+}
+
+export function SelectObject(e) {
 	const object = e.target.closest(".node")
-	const object_name = object.querySelector("span").innerHTML
-	const id = Number(object.id.split("-")[1])
-	let availability = -1
-	let optional = false
+
+	let availability
 	if (object.classList.contains("local"))
 		availability = 0
 	else if (object.classList.contains("global"))
@@ -18,22 +41,18 @@ function select_obejct(e) {
 	else if (object.classList.contains("general"))
 		availability = 2
 
-	if (object.classList.contains("optional"))
-		optional = true
-
-
 	window.opener.postMessage(
 		JSON.stringify({
-			"name": object_name,
-			"id": id,
-			"availability": availability,
-			"optional": optional
+			name: object.querySelector("span").innerHTML,
+			id: Number(object.id.split("-")[1]),
+			select: true,
+			optional: object.classList.contains("optional")
 		}),
 		window.opener.location.href)
 	window.close()
 }
 
-function expand_collapse_node_children(e) {
+export function ExpandCollapseNodeChildren(e) {
 	e.target.closest(".img-block").classList.toggle("hidden")
 	e.target.closest(".node").querySelector(".node-children").classList.toggle("hidden")
 
@@ -46,17 +65,31 @@ function expand_collapse_node_children(e) {
 	e.target.closest(".node").querySelector(another_btn_class_name).classList.toggle("hidden")
 }
 
-document.querySelectorAll(".node:not(.group) > .node-name").forEach((i) => {
-	i.addEventListener("dblclick", select_obejct)
+document.querySelectorAll(".node:not(.group) > .node-label .node-label__name").forEach((i) => {
+	i.addEventListener("dblclick", SelectObject)
 })
 
-
 expand_children.forEach((i) => {
-	i.addEventListener('click', expand_collapse_node_children)
+	i.addEventListener('click', ExpandCollapseNodeChildren)
 })
 
 collapse_children.forEach((i) => {
-	i.addEventListener('click', expand_collapse_node_children)
+	i.addEventListener('click', ExpandCollapseNodeChildren)
 })
 
 document.querySelector(".tree-actions button:first-child").addEventListener("click", update_state)
+
+document.querySelectorAll(".node-actions").forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		deleting_object = Number(e.target.closest(".node").id.split('-')[1])
+	})
+})
+
+document.querySelectorAll(".node-actions .edit").forEach(edit_btn => edit_btn.addEventListener("click", (e) => {
+    action = "edit"
+    editing_var = e.target.closest(".node").id.split("-")[1]
+}))
+document.querySelector(".tree-actions .btn:first-child").addEventListener("click", (e) => {
+    action = "create"
+    editing_var = null
+})
