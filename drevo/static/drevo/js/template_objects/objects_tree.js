e0e43aafdca5703299f6ed0e99692187942685eb
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-01 15:00:09 +0300

    Удален код, связанный с запросами на сервер. Оставлены только обработчики событий, не создающие запрос.

--- drevo/static/drevo/js/template_objects/objects_tree.js
+++ drevo/static/drevo/js/template_objects/objects_tree.js
@@ -1,11 +1,8 @@
-import {ObjectProcessingBody, GroupProcessingBody, csrftoken} from "./setup_queries.js"
-import {update_state} from "./dynamic_form.js"
 
 const url = window.location.href.split("document-template")[0] + "document-template"
 const message_block = document.querySelector(".log-container")
 const expand_children = document.querySelectorAll(".node__expand-btn")
 const collapse_children = document.querySelectorAll(".node__collapse-btn")
-const object_template = document.querySelector(".node.clone")
 const objects_contaning_list = document.querySelector(".objects-tree__containing-list")
 
 function select_obejct(e) {
@@ -53,73 +50,6 @@ document.querySelectorAll(".node:not(.group) > .node-name").forEach((i) => {
 	i.addEventListener("dblclick", select_obejct)
 })
 
-document.querySelectorAll(".edit-menu__save-btn").forEach((i) => i.addEventListener('click', (e) => {
-	let body
-	if (e.target.closest("#ObjectModal"))
-		body = ObjectProcessingBody("create", null)
-	else
-		body = GroupProcessingBody()
-
-	fetch(url + "/document_object_processing", {"method": "post", "body": body, "headers": {"X-CSRFToken": csrftoken}})
-	.then((response) => { return response.json() })
-	.then((ans) => {
-		let message = document.createElement("p")
-		message.classList.add("log-container__log")
-		if (ans["res"] == "ok") {
-			message.innerHTML = "Объект создан"
-			const object = object_template.cloneNode(true)
-			let parent
-			if (ans["object"].connected_to) {
-				parent = document.querySelector(`.node#id-${ans["object"].connected_to}`)
-				if (parent.classList.contains("leaf")) {
-					parent.classList.remove("leaf")
-					const ul = document.createElement("ul")
-					ul.classList.add("node-children")
-					parent.appendChild(ul)
-					parent = ul
-				} else {
-					parent = parent.querySelector(".node-children")
-				}
-				object.classList.add("child-node")
-			} else {
-				parent = objects_contaning_list
-			}
-			if (ans["object"].is_main) {
-				object.classList.add("group")
-			} else {
-				object.querySelector(".node-name").addEventListener("dblclick", select_obejct)
-			}
-
-			if (ans["object"].optional) {
-				object.classList.add("optional")
-			}
-			
-			object.classList.add("leaf")
-			object.setAttribute("id", `id-${ans["object"].id}`)
-
-			object.querySelector('.node-name span').innerHTML = (ans["object"].name)
-			object.querySelector(".node__expand-btn").addEventListener("click", expand_collapse_node_children)
-			object.querySelector(".node__collapse-btn").addEventListener("click", expand_collapse_node_children)
-			object.classList.remove("clone")
-			parent.appendChild(object)
-
-		} else if (ans["res"] === "validation error") {
-			message.innerHTML = ans["errors"]["__all__"][0]
-		}
-		message_block.insertBefore(message, message_block.firstChild)
-		setTimeout(() => {
-			message_block.style.display = "block"
-			message.style.opacity = "100%"
-			setTimeout(() => {
-				message.style.opacity = "0%"
-				setTimeout(() => {
-					message.remove()
-					message_block.style.display = "none"
-				}, 510)
-			}, 1500)
-		}, 10)
-	})
-}))
 
 expand_children.forEach((i) => {
 	i.addEventListener('click', expand_collapse_node_children)
