a3be6f55c30066f561b91a67a68373259db0b75e
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-03 19:12:11 +0300

    Исправлена вставка необязательного объекта в шаблон текста

--- drevo/static/drevo/js/template_objects/document_text_template.js
+++ drevo/static/drevo/js/template_objects/document_text_template.js
@@ -28,19 +28,21 @@ window.addEventListener("message", (e) => {
 			focused_object_header.innerHTML = data.name
 			focused_object.id = data.id
 			focused_object_block.id = data.id
-			if (data.optional && !focused_object_block.classList.contains("optional"))
-				focused_object_block.classList.add("optional")
-			else if (!data.optional && focused_object_block.classList.contains("optional"))
-				focused_object_block.classList.remove("optional")
-
-			selected_object = {name: data.name, id: data.id, optional: data.optional}
 		} else if (data.id == focused_object.id) {
 			focused_object_header.innerHTML = data.name
 		}
 
+		selected_object = {name: data.name, id: data.id, optional: data.optional}
+
+		if (data.optional && !focused_object_block.classList.contains("optional"))
+			focused_object_block.classList.add("optional")
+		else if (!data.optional && focused_object_block.classList.contains("optional"))
+			focused_object_block.classList.remove("optional")
+
 		if (document.readyState === 'complete') {
 			const editor = CKEDITOR.instances.id_content
 			const objects_in_text = editor.document.$.body.querySelectorAll(`.template-object#id-${data.id}`)
+
 			objects_in_text.forEach((object) => {
 				object.innerHTML = `&lt;${data.name}&gt;`
 			})
@@ -67,17 +69,17 @@ document.addEventListener("DOMContentLoaded", (e) => {
 paste_btn.addEventListener("click", (e) => {
 	if (document.readyState === 'complete') {
 
-		console.log(selected_object)
-
 		if (selected_object.optional) {
-			let object_html_code = `<span contenteditable="false" id="${selected_object.id}">{<span contenteditable="true">&nbsp;</span><span>[${selected_object.name}]</span>}</span>`
+			const object_html_code = `<span contenteditable="false">{[<span contenteditable="true">&nbsp;</span>]<span class="template-object" id="id-${selected_object.id}" >${selected_object.name}</span>}</span><span>&nbsp;</span>`
   			console.log(object_html_code)
   			CKEDITOR.instances.id_content.insertHtml(object_html_code)
 		} else {
-			let element = CKEDITOR.dom.element.createFromHtml(`<span class="template-object" id="id-${selected_object.id}" contenteditable="false">&lt;${selected_object.name}&gt;</span>`)
-			let space = CKEDITOR.dom.element.createFromHtml("<span>&nbsp;</span>")
+			const element = CKEDITOR.dom.element.createFromHtml(`<span class="template-object" id="id-${selected_object.id}" contenteditable="false">&lt;${selected_object.name}&gt;</span>`)
 			CKEDITOR.instances.id_content.insertElement(element)
+
+			const space = CKEDITOR.dom.element.createFromHtml("<span>&nbsp;</span>")
 			CKEDITOR.instances.id_content.insertElement(space)
 		}
+
 	}
 })
