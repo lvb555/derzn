54fa77e6953910963b0cb2803b9ad426f61424f4
RomanBorovskiy <borovskiyroman@gmail.com>
2024-09-15 21:35:44 +0300

    Удаление закомментированного кода

--- drevo/views/knowledge_view.py
+++ drevo/views/knowledge_view.py
@@ -20,20 +20,11 @@ class KnowledgeView(TemplateView):
         Передает данные в шаблон
         """
         context = super().get_context_data(**kwargs)
-        # формирует список категорий
-        # categories = Category.tree_objects.exclude(is_published=False)
-        # context['ztypes'] = categories
 
         # формирование списка Знаний по категориям
         zn = Znanie.published.all().prefetch_related('tz', 'author')
-        context['zn_queryset'] = zn
-        # zn_dict = {}
-        # for category in categories:
-        #     zn_in_this_category = zn.filter(
-        #         category=category).order_by('-order')
-        #     zn_dict[category.name] = zn_in_this_category
-        # context['zn_dict'] = zn_dict
 
+        context['zn_queryset'] = zn
         return context
 
 
