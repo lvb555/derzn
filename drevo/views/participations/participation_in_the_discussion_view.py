32ee366df309f6dd678d76236c86fd0411f02292
KudryashovR <romkud55@gmail.com>
2024-10-21 21:51:50 +0300

    FIX: Отсутствие отображения темы дисскуссии при отсутствии дерева для отображения

--- drevo/views/participations/participation_in_the_discussion_view.py
+++ drevo/views/participations/participation_in_the_discussion_view.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from django.views.generic import DetailView
 
 from drevo.models import Znanie
@@ -17,7 +15,5 @@ class ParticipationInTheDiscussionView(DetailView):
         knowledge = Znanie.objects.get(pk=pk)
         context['znanie'] = knowledge
         context['relative_znania'] = get_descendants_for_knowledge(knowledge)
-
-        pprint(context['relative_znania'])
 
         return context
